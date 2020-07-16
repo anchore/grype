31e124147641726dcbac16ae1629da3b5e057e42
Alex Goodman <alex.goodman@anchore.com>
2020-07-16 13:35:22 -0400

    test support for CPE URI bindings
    addresses https://github.com/anchore/vulnscan/pull/40#discussion_r455389937

--- vulnscan/cpe/cpe_test.go
+++ vulnscan/cpe/cpe_test.go
@@ -13,6 +13,45 @@ func must(c CPE, e error) CPE {
 	return c
 }
 
+
+func TestNew(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected CPE
+	}{
+		{
+			name: "gocase",
+			input: `cpe:/a:10web:form_maker:1.0.0::~~~wordpress~~`,
+			expected: must(New(`cpe:2.3:a:10web:form_maker:1.0.0:*:*:*:*:wordpress:*:*`)),
+		},
+		{
+			name: "dashes",
+			input: `cpe:/a:7-zip:7-zip:4.56:beta:~~~windows~~`,
+			expected: must(New(`cpe:2.3:a:7-zip:7-zip:4.56:beta:*:*:*:windows:*:*`)),
+		},
+		{
+			name: "URL escape characters",
+			input: `cpe:/a:%240.99_kindle_books_project:%240.99_kindle_books:6::~~~android~~`,
+			expected: must(New(`cpe:2.3:a:$0.99_kindle_books_project:$0.99_kindle_books:6:*:*:*:*:android:*:*`)),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual, err := New(test.input)
+			if err != nil {
+				t.Fatalf("got an error while creating CPE: %+v", err)
+			}
+
+			if actual.BindToFmtString() != test.expected.BindToFmtString() {
+				t.Errorf("mismatched entries:\n\texpected:%+v\n\t  actual:%+v\n", test.expected.BindToFmtString(), actual.BindToFmtString())
+			}
+
+		})
+	}
+}
+
 func TestGenerate(t *testing.T) {
 	tests := []struct {
 		name     string
