3da2ff81a72eaf158ab25d8b460895e7ec8fcb6a
Alfredo Deza <adeza@anchore.com>
2020-07-22 08:27:58 -0400

    matchers: include python-requirements matching
    Signed-off-by: Alfredo Deza <adeza@anchore.com>

--- vulnscan/matcher/python/matcher.go
+++ vulnscan/matcher/python/matcher.go
@@ -12,7 +12,7 @@ type Matcher struct {
 }
 
 func (m *Matcher) PackageTypes() []pkg.Type {
-	return []pkg.Type{pkg.EggPkg, pkg.WheelPkg}
+	return []pkg.Type{pkg.EggPkg, pkg.WheelPkg, pkg.PythonRequirementsPkg}
 }
 
 func (m *Matcher) Type() match.MatcherType {
