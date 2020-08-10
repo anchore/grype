219d8bcf0f2c519f9d14a1d66e0d46563df7c9ad
Dan Luhring <luhring@users.noreply.github.com>
2020-08-10 16:03:12 -0400

    Use warn instead of error for packages with no matchers (#113)
    * Add warn method to logging system
    
    Signed-off-by: Dan Luhring <dan.luhring@anchore.com>
    
    * Move from error to warn for no matcher scenario
    
    Signed-off-by: Dan Luhring <dan.luhring@anchore.com>

--- grype/logger/logger.go
+++ grype/logger/logger.go
@@ -2,6 +2,7 @@ package logger
 
 type Logger interface {
 	Errorf(format string, args ...interface{})
+	Warnf(format string, args ...interface{})
 	Infof(format string, args ...interface{})
 	Info(args ...interface{})
 	Debugf(format string, args ...interface{})
