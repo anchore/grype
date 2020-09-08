709c2d9a7d1459b9976a12bd5915a5ff7c653805
Alex Goodman <alex.goodman@anchore.com>
2020-09-08 11:01:01 -0400

    add documentation around the match object
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>

--- grype/match/match.go
+++ grype/match/match.go
@@ -7,23 +7,24 @@ import (
 	"github.com/anchore/syft/syft/pkg"
 )
 
+// Match represents a finding in the vulnerability matching process, pairing a single package and a single vulnerability object.
 type Match struct {
-	Type          Type
-	Confidence    float64
-	Vulnerability vulnerability.Vulnerability
-	Package       *pkg.Package
-	// SearchKey provides an indication of how this match was found.
-	// TODO: is this a good name for what it represents? (which is an audit trail of HOW we got this match from the store)
-	SearchKey       map[string]interface{}
-	SearchMatches   map[string]interface{}
-	IndirectPackage *pkg.Package
-	Matcher         MatcherType
+	Type            Type                        // The kind of match made (an exact match, fuzzy match, indirect vs direct, etc).
+	Confidence      float64                     // The certainty of the match as a ratio (currently unused, reserved for future use).
+	Vulnerability   vulnerability.Vulnerability // The vulnerability details of the match.
+	Package         *pkg.Package                // The package used to search for a match.
+	SearchKey       map[string]interface{}      // The specific attributes that were used to search (other than package name and version) --this indicates "how" the match was made.
+	SearchMatches   map[string]interface{}      // The specific attributes on the vulnerability object that were matched with --this indicates "what" was found in the match.
+	IndirectPackage *pkg.Package                // An optional package which was used to indirectly match the package and vulnerability. This is used when the OS package only matches with the development/source package for vulnerability matching.
+	Matcher         MatcherType                 // The matcher object that discovered the match.
 }
 
+// String is the string representation of select match fields.
 func (m Match) String() string {
 	return fmt.Sprintf("Match(pkg=%s vuln=%s type='%s' foundBy='%s')", m.Package, m.Vulnerability.String(), m.Type, m.Matcher)
 }
 
+// Summary is a short string representation of the match object.
 func (m Match) Summary() string {
 	return fmt.Sprintf("vuln='%s' type='%s' key='%s' foundBy='%s'", m.Vulnerability.ID, m.Type, m.SearchKey, m.Matcher)
 }
