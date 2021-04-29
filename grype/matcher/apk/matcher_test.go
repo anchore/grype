33b59d544eab16f31a43c93ca08237ce1dcbd840
Alex Goodman <alex.goodman@anchore.com>
2021-04-29 09:27:04 -0400

    add apk matcher case for mismatched product and package names
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>

--- grype/matcher/apk/matcher_test.go
+++ grype/matcher/apk/matcher_test.go
@@ -168,8 +168,81 @@ func TestBothSecdbAndNvdMatches(t *testing.T) {
 	}
 }
 
-func TestNvdOnlyMatches(t *testing.T) {
+func TestBothSecdbAndNvdMatches_DifferentPackageName(t *testing.T) {
 	// NVD and Alpine's secDB both have the same CVE ID for the package
+	nvdVuln := db.Vulnerability{
+		ID:                "CVE-2020-1",
+		VersionConstraint: "<= 0.9.11",
+		VersionFormat:     "unknown",
+		// Note: the product name is NOT the same as the target package name
+		CPEs: []string{"cpe:2.3:a:lib_vnc_project-(server):libvncumbrellaproject:*:*:*:*:*:*:*:*"},
+	}
+
+	secDbVuln := db.Vulnerability{
+		// ID *does* match - this is the key for comparison in the matcher
+		ID:                "CVE-2020-1",
+		VersionConstraint: "<= 0.9.11",
+		VersionFormat:     "apk",
+	}
+	store := mockStore{
+		backend: map[string]map[string][]db.Vulnerability{
+			"nvd": {
+				"libvncumbrellaproject": []db.Vulnerability{nvdVuln},
+			},
+			"alpine:3.12": {
+				"libvncserver": []db.Vulnerability{secDbVuln},
+			},
+		},
+	}
+
+	provider := vulnerability.NewProviderFromStore(&store)
+
+	m := Matcher{}
+	d, err := distro.NewDistro(distro.Alpine, "3.12.0", "")
+	if err != nil {
+		t.Fatalf("failed to create a new distro: %+v", err)
+	}
+	p := pkg.Package{
+		Name:    "libvncserver",
+		Version: "0.9.9",
+		CPEs: []syftPkg.CPE{
+			// Note: the product name is NOT the same as the package name
+			must(syftPkg.NewCPE("cpe:2.3:a:*:libvncumbrellaproject:0.9.9:*:*:*:*:*:*:*")),
+		},
+	}
+
+	// ensure the SECDB record is preferred over the NVD record
+	vulnFound, err := vulnerability.NewVulnerability(secDbVuln)
+	assert.NoError(t, err)
+
+	expected := []match.Match{
+		{
+			Type:          match.ExactDirectMatch,
+			Confidence:    1.0,
+			Vulnerability: *vulnFound,
+			Package:       p,
+			SearchKey: map[string]interface{}{
+				"distro": map[string]string{
+					"type":    d.Type.String(),
+					"version": d.RawVersion,
+				},
+			},
+			SearchMatches: map[string]interface{}{
+				"constraint": vulnFound.Constraint.String(),
+			},
+			Matcher: match.ApkMatcher,
+		},
+	}
+
+	actual, err := m.Match(provider, &d, p)
+	assert.NoError(t, err)
+
+	for _, diff := range deep.Equal(expected, actual) {
+		t.Errorf("diff: %+v", diff)
+	}
+}
+
+func TestNvdOnlyMatches(t *testing.T) {
 	nvdVuln := db.Vulnerability{
 		ID:                "CVE-2020-1",
 		VersionConstraint: "<= 0.9.11",
