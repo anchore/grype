5f754eb995820d25a615f2dd514dcb6a4a0e2707
Christopher Phillips <cphillips918@gmail.com>
2021-08-19 16:15:53 -0400

    update regex based on PR feedback
    Signed-off-by: Christopher Phillips <cphillips918@gmail.com>

--- grype/matcher/rpmdb/matcher.go
+++ grype/matcher/rpmdb/matcher.go
@@ -17,7 +17,7 @@ import (
 // the source-rpm field has something akin to "util-linux-ng-2.17.2-12.28.el6_9.2.src.rpm"
 // in which case the pattern will extract out "util-linux-ng" as the left-most capture group
 // name, version, release, epoch, arch
-var rpmPackageNamePattern = regexp.MustCompile(`(?P<name>.*)-(?P<version>.*)-(?P<release>.*?)\.(?P<arch>.*)(\.rpm)`)
+var rpmPackageNamePattern = regexp.MustCompile(`^(?P<name>.*)-(?P<version>.*)-(?P<release>.*?)\.(?P<arch>.*)(\.rpm)$`)
 
 type Matcher struct {
 }
