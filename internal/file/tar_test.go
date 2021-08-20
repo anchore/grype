168200849b36b3ff1f187e25ce55a048c939805d
Alex Goodman <alex.goodman@anchore.com>
2021-08-20 11:33:56 -0400

    bump untar file size threshold
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>

--- internal/file/tar_test.go
+++ internal/file/tar_test.go
@@ -1,8 +1,10 @@
 package file
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -79,6 +81,58 @@ func TestSafeJoin(t *testing.T) {
 			actual, err := safeJoin(test.prefix, test.args...)
 			test.errAssertion(t, err)
 			assert.Equal(t, test.expected, actual)
+		})
+	}
+}
+
+func Test_copyWithLimits(t *testing.T) {
+	tests := []struct {
+		name          string
+		input         string
+		byteReadLimit int64
+		target        string
+		expectWritten string
+		expectErr     bool
+	}{
+		{
+			name:          "write bytes",
+			input:         "something here",
+			byteReadLimit: 1000,
+			target:        "dont care",
+			expectWritten: "something here",
+			expectErr:     false,
+		},
+		{
+			name:          "surpass upper limit",
+			input:         "something here",
+			byteReadLimit: 11,
+			target:        "dont care",
+			expectWritten: "something h",
+			expectErr:     true,
+		},
+		// since we want the threshold being reached to be easily detectable, simply reaching the threshold is
+		// enough to cause an error. Otherwise surpassing the threshold would be undetectable.
+		{
+			name:          "reach limit exactly",
+			input:         "something here",
+			byteReadLimit: 14,
+			target:        "dont care",
+			expectWritten: "something here",
+			expectErr:     true,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			writer := &bytes.Buffer{}
+			err := copyWithLimits(writer, strings.NewReader(test.input), test.byteReadLimit, test.target)
+			if (err != nil) != test.expectErr {
+				t.Errorf("copyWithLimits() error = %v, want %v", err, test.expectErr)
+				return
+			} else if err != nil {
+				assert.Contains(t, err.Error(), test.target)
+			}
+			assert.Equal(t, test.expectWritten, writer.String())
+
 		})
 	}
 }
