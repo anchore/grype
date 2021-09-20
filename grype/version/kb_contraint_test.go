672bd5a1fbfcc179a3782d7ac1cb462ba6b20ea1
Vijay Pillai <vijay.pillai@anchore.com>
2021-09-20 12:02:49 -0400

    add test to kb_constraint_test for version "base" and constraint "base"
    Signed-off-by: Vijay Pillai <vijay.pillai@anchore.com>

--- grype/version/kb_contraint_test.go
+++ grype/version/kb_contraint_test.go
@@ -14,6 +14,7 @@ func TestVersionKbConstraint(t *testing.T) {
 		{version: "878787", constraint: "", satisfied: false, checkErrType: expectedErrorType},
 		{version: "1", constraint: "foo", satisfied: false},
 		{version: "1", constraint: "1", satisfied: true},
+		{version: "base", constraint: "base", satisfied: true},
 		{version: "878787", constraint: "979797 || 101010 || 878787", satisfied: true},
 		{version: "478787", constraint: "979797 || 101010 || 878787", satisfied: false},
 	}
