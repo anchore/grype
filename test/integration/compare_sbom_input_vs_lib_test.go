1a7c9d177904756b820cea1044c8a5c452d8a4c3
Vijay Pillai <vijay.pillai@anchore.com>
2021-09-22 21:53:32 -0400

    Bugfixes + Integration test for sbom input vs grype library comparison (#424)
    This change both adds a test to identify and fixes differences between loading sboms from json and loading sboms from Syft as a library.
    * adds integration test that compares SBOM input vs image input
    * fix integration test cache path
    * Add handler for ApkMetadataType in partialSyftPackage.UnmarshalJSON
    * Fix Epoch missing from Package.New RpmdbMetadataType handler and update RpmDbMetadata test in TestNew_MetadataExtraction
    * bump syft to version 0.24.0
    * update license check for packageurl-go
    
    Co-authored-by: Alex Goodman <alex.goodman@anchore.com>
    Co-authored-by: Vijay Pillai <vijay.pillai@anchore.com>
    
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>
    Signed-off-by: Vijay Pillai <vijay.pillai@anchore.com>

---
+++ test/integration/compare_sbom_input_vs_lib_test.go
@@ -0,0 +1,92 @@
+package integration
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/anchore/grype/grype"
+	"github.com/anchore/grype/grype/db"
+	"github.com/anchore/grype/internal"
+	syftPkg "github.com/anchore/syft/syft/pkg"
+	"github.com/anchore/syft/syft/source"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/scylladb/go-set/strset"
+)
+
+func TestCompareSBOMInputToLibResults(t *testing.T) {
+	images := []string{
+		"anchore/test_images:vulnerabilities-alpine",
+		"anchore/test_images:gems",
+		"anchore/test_images:vulnerabilities-debian",
+		"anchore/test_images:vulnerabilities-centos",
+		"anchore/test_images:npm",
+		"anchore/test_images:java",
+		"anchore/test_images:golang",
+	}
+
+	// get a grype DB
+	vulnProvider, _, _, err := grype.LoadVulnerabilityDb(db.Config{
+		DbRootDir:           "test-fixtures/grype-db",
+		ListingURL:          internal.DBUpdateURL,
+		ValidateByHashOnGet: false,
+	}, true)
+	assert.NoError(t, err)
+
+	definedPkgTypes := strset.New()
+	for _, p := range syftPkg.AllPkgs {
+		definedPkgTypes.Add(string(p))
+	}
+	// exceptions: rust and msrc (kb) are not under test
+	definedPkgTypes.Remove(string(syftPkg.RustPkg), string(syftPkg.KbPkg))
+	observedPkgTypes := strset.New()
+
+	for _, image := range images {
+		t.Run(image, func(t *testing.T) {
+
+			t.Logf("Running case %s", image)
+
+			imageArchive := PullThroughImageCache(t, image)
+			imageSource := fmt.Sprintf("docker-archive:%s", imageArchive)
+
+			// get SBOM from syft, write to temp file
+			sbomBytes := getSyftSBOM(t, imageSource)
+			sbomFile, err := ioutil.TempFile("", "")
+			assert.NoError(t, err)
+			t.Cleanup(func() {
+				assert.NoError(t, os.Remove(sbomFile.Name()))
+			})
+			_, err = sbomFile.WriteString(sbomBytes)
+			assert.NoError(t, err)
+			assert.NoError(t, sbomFile.Close())
+
+			// get vulns (sbom)
+			matchesFromSbom, _, pkgsFromSbom, err := grype.FindVulnerabilities(vulnProvider, fmt.Sprintf("sbom:%s", sbomFile.Name()), source.SquashedScope, nil)
+			assert.NoError(t, err)
+
+			// get vulns (image)
+			matchesFromImage, _, _, err := grype.FindVulnerabilities(vulnProvider, imageSource, source.SquashedScope, nil)
+			assert.NoError(t, err)
+
+			// compare packages (shallow)
+			matchSetFromSbom := getMatchSet(matchesFromSbom)
+			matchSetFromImage := getMatchSet(matchesFromImage)
+
+			assert.Empty(t, strset.Difference(matchSetFromSbom, matchSetFromImage).List(), "vulnerabilities present only in results when using sbom as input")
+			assert.Empty(t, strset.Difference(matchSetFromImage, matchSetFromSbom).List(), "vulnerabilities present only in results when using image as input")
+
+			// track all covered package types (for use after the test)
+			for _, p := range pkgsFromSbom {
+				observedPkgTypes.Add(string(p.Type))
+			}
+
+		})
+	}
+
+	// ensure we've covered all package types (-rust, -kb)
+	unobservedPackageTypes := strset.Difference(definedPkgTypes, observedPkgTypes)
+	assert.Empty(t, unobservedPackageTypes.List(), "not all package type were covered in testing")
+
+}
---
+++ test/integration/utils_test.go
@@ -0,0 +1,95 @@
+package integration
+
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"fmt"
+	"github.com/anchore/grype/grype/match"
+	"github.com/scylladb/go-set/strset"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+	"testing"
+
+	"github.com/anchore/syft/syft"
+	"github.com/anchore/syft/syft/presenter/packages"
+	"github.com/anchore/syft/syft/source"
+)
+
+const cacheDirRelativePath string = "./test-fixtures/cache"
+
+func PullThroughImageCache(t testing.TB, imageName string) string {
+	cacheDirectory, absErr := filepath.Abs(cacheDirRelativePath)
+	if absErr != nil {
+		t.Fatalf("could not get absolute path of cache directory %s; %v", cacheDirRelativePath, absErr)
+	}
+
+	mkdirError := os.MkdirAll(cacheDirectory, 0755)
+	if mkdirError != nil {
+		t.Fatalf("could not create cache directory %s; %v", cacheDirRelativePath, absErr)
+	}
+
+	re := regexp.MustCompile("[/:]")
+	archiveFileName := fmt.Sprintf("%s.tar", re.ReplaceAllString(imageName, "-"))
+	imageArchivePath := filepath.Join(cacheDirectory, archiveFileName)
+
+	if _, err := os.Stat(imageArchivePath); os.IsNotExist(err) {
+		t.Logf("Cache miss for image %s; copying to archive at %s", imageName, imageArchivePath)
+		saveImage(t, imageName, imageArchivePath)
+	}
+
+	return imageArchivePath
+}
+
+func saveImage(t testing.TB, imageName string, destPath string) {
+	sourceImage := fmt.Sprintf("docker://docker.io/%s", imageName)
+	destinationString := fmt.Sprintf("docker-archive:%s", destPath)
+
+	cmd := exec.Command("skopeo", "copy", "--override-os", "linux", sourceImage, destinationString)
+
+	out, err := cmd.Output()
+	if err != nil {
+		var exitError *exec.ExitError
+		if errors.As(err, &exitError) {
+			t.Logf("Stderr: %s", exitError.Stderr)
+		}
+		t.Fatal(err)
+	}
+
+	t.Logf("Stdout: %s\n", out)
+}
+
+func getSyftSBOM(t testing.TB, image string) string {
+	src, cleanup, err := source.New(image, nil)
+	if err != nil {
+		t.Fatalf("can't get the source: %+v", err)
+	}
+	t.Cleanup(cleanup)
+
+	scope := source.SquashedScope
+	catalog, distro, err := syft.CatalogPackages(src, scope)
+
+	presenter := packages.Presenter(packages.JSONPresenterOption, packages.PresenterConfig{
+		SourceMetadata: src.Metadata,
+		Catalog:        catalog,
+		Distro:         distro,
+		Scope:          scope,
+	})
+
+	var buf bytes.Buffer
+	if err := presenter.Present(bufio.NewWriter(&buf)); err != nil {
+		t.Fatalf("presenter failed: %+v", err)
+	}
+
+	return buf.String()
+}
+
+func getMatchSet(matches match.Matches) *strset.Set {
+	s := strset.New()
+	for _, m := range matches.Sorted() {
+		s.Add(fmt.Sprintf("%s-%s-%s-%s", m.Vulnerability.ID, m.Package.Name, m.Package.Version, string(m.Package.Type)))
+	}
+	return s
+}
