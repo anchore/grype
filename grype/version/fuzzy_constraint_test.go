6919ef145ca8b5031d427172cf9d4bd4ca2556ce
Christopher Angelo Phillips <christopher.phillips@anchore.com>
2021-10-01 10:21:48 -0400

    merge upstream
    Signed-off-by: Christopher Angelo Phillips <christopher.phillips@anchore.com>

--- grype/version/fuzzy_constraint_test.go
+++ grype/version/fuzzy_constraint_test.go
@@ -244,25 +244,25 @@ func TestFuzzyConstraintSatisfaction(t *testing.T) {
 			name:       "strip unbalanced v from left side <",
 			version:    "v17.12.0-ce-rc1.0.20200309214505-aa6a9891b09c+incompatible",
 			constraint: "< 1.5",
-			expected:   false,
+			satisfied:  false,
 		},
 		{
 			name:       "strip unbalanced v from left side >",
 			version:    "v17.12.0-ce-rc1.0.20200309214505-aa6a9891b09c+incompatible",
 			constraint: "> 1.5",
-			expected:   true,
+			satisfied:  true,
 		},
 		{
 			name:       "strip unbalanced v from right side <",
 			version:    "17.12.0-ce-rc1.0.20200309214505-aa6a9891b09c+incompatible",
 			constraint: "< v1.5",
-			expected:   false,
+			satisfied:  false,
 		},
 		{
 			name:       "strip unbalanced v from right side >",
 			version:    "17.12.0-ce-rc1.0.20200309214505-aa6a9891b09c+incompatible",
 			constraint: "> v1.5",
-			expected:   true,
+			satisfied:  true,
 		},
 	}
 
