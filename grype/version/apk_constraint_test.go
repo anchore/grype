fb06d93c8736aefba227ca4947720918f4b19a93
Christopher Angelo Phillips <christopher.phillips@anchore.com>
2021-10-15 10:19:31 -0400

    add test for new apk version
    Signed-off-by: Christopher Angelo Phillips <christopher.phillips@anchore.com>

---
+++ grype/version/apk_constraint_test.go
@@ -0,0 +1,48 @@
+package version
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestVersionApk(t *testing.T) {
+	tests := []testCase{
+		{version: "2.3.1", constraint: "", satisfied: true},
+		// compound conditions
+		{version: "2.3.1", constraint: "> 1.0.0, < 2.0.0", satisfied: false},
+		{version: "1.3.1", constraint: "> 1.0.0, < 2.0.0", satisfied: true},
+		{version: "2.0.0", constraint: "> 1.0.0, <= 2.0.0", satisfied: true},
+		{version: "2.0.0", constraint: "> 1.0.0, < 2.0.0", satisfied: false},
+		{version: "1.0.0", constraint: ">= 1.0.0, < 2.0.0", satisfied: true},
+		{version: "1.0.0", constraint: "> 1.0.0, < 2.0.0", satisfied: false},
+		{version: "0.9.0", constraint: "> 1.0.0, < 2.0.0", satisfied: false},
+		{version: "1.5.0", constraint: "> 0.1.0, < 0.5.0 || > 1.0.0, < 2.0.0", satisfied: true},
+		{version: "0.2.0", constraint: "> 0.1.0, < 0.5.0 || > 1.0.0, < 2.0.0", satisfied: true},
+		{version: "0.0.1", constraint: "> 0.1.0, < 0.5.0 || > 1.0.0, < 2.0.0", satisfied: false},
+		{version: "0.6.0", constraint: "> 0.1.0, < 0.5.0 || > 1.0.0, < 2.0.0", satisfied: false},
+		{version: "2.5.0", constraint: "> 0.1.0, < 0.5.0 || > 1.0.0, < 2.0.0", satisfied: false},
+		// fixed-in scenarios
+		{version: "2.3.1", constraint: "< 2.0.0", satisfied: false},
+		{version: "2.3.1", constraint: "< 2.0", satisfied: false},
+		{version: "2.3.1", constraint: "< 2", satisfied: false},
+		{version: "2.3.1", constraint: "< 2.3", satisfied: false},
+		{version: "2.3.1", constraint: "< 2.3.1", satisfied: false},
+		{version: "2.3.1", constraint: "< 2.3.2", satisfied: true},
+		{version: "2.3.1", constraint: "< 2.4", satisfied: true},
+		{version: "2.3.1", constraint: "< 3", satisfied: true},
+		{version: "2.3.1", constraint: "< 3.0", satisfied: true},
+		{version: "2.3.1", constraint: "< 3.0.0", satisfied: true},
+		{version: "1.5.1-r1", constraint: "< 1.5.1", satisfied: false},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			constraint, err := newApkConstraint(test.constraint)
+
+			assert.NoError(t, err, "unexpected error from newApkConstraint: %v", err)
+			test.assertVersionConstraint(t, ApkFormat, constraint)
+
+		})
+	}
+}
