0487e210c978c50cb8f640fde8e6009e2796b9fe
Christopher Angelo Phillips <christopher.phillips@anchore.com>
2021-10-18 13:14:24 -0400

    add more alpine specific scenario
    Signed-off-by: Christopher Angelo Phillips <christopher.phillips@anchore.com>

--- grype/version/apk_constraint_test.go
+++ grype/version/apk_constraint_test.go
@@ -33,7 +33,18 @@ func TestVersionApk(t *testing.T) {
 		{version: "2.3.1", constraint: "< 3", satisfied: true},
 		{version: "2.3.1", constraint: "< 3.0", satisfied: true},
 		{version: "2.3.1", constraint: "< 3.0.0", satisfied: true},
+		// alpine specific scenarios
+		// https://wiki.alpinelinux.org/wiki/APKBUILD_Reference#pkgver
 		{version: "1.5.1-r1", constraint: "< 1.5.1", satisfied: false},
+		{version: "1.5.1-r1", constraint: "> 1.5.1", satisfied: true},
+		{version: "9.3.2-r4", constraint: "< 9.3.4-r2", satisfied: true},
+		{version: "9.3.4-r2", constraint: "> 9.3.4", satisfied: true},
+		{version: "4.2.52_p2-r1", constraint: "< 4.2.52_p4-r2", satisfied: true},
+		{version: "4.2.52_p2-r1", constraint: "> 4.2.52_p4-r2", satisfied: false},
+		{version: "0.1.0_alpha", constraint: "< 0.1.3_alpha", satisfied: true},
+		{version: "0.1.0_alpha2", constraint: "> 0.1.0_alpha", satisfied: true},
+		{version: "1.1", constraint: "> 1.1_alpha1", satisfied: true},
+		{version: "1.1", constraint: "< 1.1_alpha1", satisfied: false},
 	}
 
 	for _, test := range tests {
