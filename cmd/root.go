fb2b5cae896b39c8a993fd2fe8c61d939705a2a5
Alex Goodman <alex.goodman@anchore.com>
2021-10-20 09:47:53 -0400

    factor out verbosity check to function
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>

--- cmd/root.go
+++ cmd/root.go
@@ -178,23 +178,24 @@ func rootExec(_ *cobra.Command, args []string) error {
 		return err
 	}
 
-	isVerbose := appConfig.CliOptions.Verbosity > 0
-	if isPipedInput, err := internal.IsPipedInput(); err != nil {
-		// since we can't tell if there was piped input we assume that there could be to disable the ETUI
-		log.Warnf("unable to determine if there is piped input: %+v", err)
-		isVerbose = true
-	} else {
-		// verbosity should consider if there is piped input (in which case we should not show the ETUI)
-		isVerbose = isVerbose || isPipedInput
-	}
-
 	return eventLoop(
 		startWorker(userInput, appConfig.FailOnSeverity),
 		setupSignals(),
 		eventSubscription,
 		stereoscope.Cleanup,
-		ui.Select(isVerbose, appConfig.Quiet, reporter)...,
+		ui.Select(isVerbose(), appConfig.Quiet, reporter)...,
 	)
+}
+
+func isVerbose() (result bool) {
+	isPipedInput, err := internal.IsPipedInput()
+	if err != nil {
+		// since we can't tell if there was piped input we assume that there could be to disable the ETUI
+		log.Warnf("unable to determine if there is piped input: %+v", err)
+		return true
+	}
+	// verbosity should consider if there is piped input (in which case we should not show the ETUI)
+	return appConfig.CliOptions.Verbosity > 0 || isPipedInput
 }
 
 // nolint:funlen
