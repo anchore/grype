9d23bcd0171b9fecb542ebe65c57739e246383a6
Alex Goodman <alex.goodman@anchore.com>
2021-10-28 10:41:57 -0400

    use named pipe bit on stdin as indicator for piped input
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>

--- internal/input.go
+++ internal/input.go
@@ -12,5 +12,9 @@ func IsPipedInput() (bool, error) {
 		return false, fmt.Errorf("unable to determine if there is piped input: %w", err)
 	}
 
-	return fi.Mode()&os.ModeCharDevice == 0, nil
+	// note: we should NOT use the absence of a character device here as the hint that there may be input expected
+	// on stdin, as running grype as a subprocess you would expect no character device to be present but input can
+	// be from either stdin or indicated by the CLI. Checking if stdin is a pipe is the most direct way to determine
+	// if there *may* be bytes that will show up on stdin that should be used for the analysis source.
+	return fi.Mode()&os.ModeNamedPipe != 0, nil
 }
