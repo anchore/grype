04f2a9b8067dd30321ce7b57d0393374111652cf
Keith Zantow <kzantow@gmail.com>
2021-12-20 14:58:33 -0500

    add image tests for exclude flag
    Signed-off-by: Keith Zantow <kzantow@gmail.com>

--- grype/pkg/provider_test.go
+++ grype/pkg/provider_test.go
@@ -1,9 +1,12 @@
 package pkg
 
 import (
+	"testing"
+
+	"github.com/anchore/stereoscope/pkg/image"
+	"github.com/anchore/stereoscope/pkg/imagetest"
 	"github.com/anchore/syft/syft/source"
 	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
 func TestProviderLocationExcludes(t *testing.T) {
@@ -28,7 +31,7 @@ func TestProviderLocationExcludes(t *testing.T) {
 		{
 			name:     "include everything with no match",
 			fixture:  "test-fixtures/syft-spring.json",
-			excludes: []string{},
+			excludes: []string{"**/asdf*.jar"},
 			expected: []string{"charsets", "tomcat-embed-el"},
 		},
 		{
@@ -42,6 +45,58 @@ func TestProviderLocationExcludes(t *testing.T) {
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			pkgs, _, _ := Provide(test.fixture, source.SquashedScope, nil, test.excludes...)
+
+			var pkgNames []string
+
+			for _, pkg := range pkgs {
+				pkgNames = append(pkgNames, pkg.Name)
+			}
+
+			assert.ElementsMatch(t, pkgNames, test.expected)
+		})
+	}
+}
+
+func TestSyftLocationExcludes(t *testing.T) {
+	tests := []struct {
+		name     string
+		fixture  string
+		excludes []string
+		expected []string
+	}{
+		{
+			name:     "exclude everything",
+			fixture:  "image-simple",
+			excludes: []string{"**"},
+			expected: []string{},
+		},
+		{
+			name:     "exclude specific real path match",
+			fixture:  "image-simple",
+			excludes: []string{"**/nested/package.json"},
+			expected: []string{"top-level-package"},
+		},
+		{
+			name:     "include everything with no match",
+			fixture:  "image-simple",
+			excludes: []string{"**/asdf*.json"},
+			expected: []string{"nested-package", "top-level-package"},
+		},
+		{
+			name:     "include everything with no excludes",
+			fixture:  "image-simple",
+			excludes: []string{},
+			expected: []string{"nested-package", "top-level-package"},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			userInput := imagetest.GetFixtureImageTarPath(t, test.fixture)
+
+			pkgs, _, err := Provide(userInput, source.SquashedScope, &image.RegistryOptions{}, test.excludes...)
+
+			assert.NoErrorf(t, err, "error calling Provide function")
 
 			var pkgNames []string
 
