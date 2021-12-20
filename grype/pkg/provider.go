d34f85d24a314a73cfd3fb794a3f03c626a85020
Keith Zantow <kzantow@gmail.com>
2021-12-20 15:09:21 -0500

    refactor for improved comprehension
    Signed-off-by: Keith Zantow <kzantow@gmail.com>

--- grype/pkg/provider.go
+++ grype/pkg/provider.go
@@ -17,28 +17,44 @@ func Provide(userInput string, scopeOpt source.Scope, registryOptions *image.Reg
 	packages, ctx, err := syftSBOMProvider(userInput)
 	if !errors.Is(err, errDoesNotProvide) {
 		if len(exclusions) > 0 {
-			for i := 0; i < len(packages); i++ {
-				pkg := packages[i]
-				for _, exclusion := range exclusions {
-					for _, location := range pkg.Locations {
-						matchesRealPath, err := doublestar.Match(exclusion, location.RealPath)
-						if err != nil {
-							return nil, ctx, err
-						}
-						matchesVirtualPath, err := doublestar.Match(exclusion, location.VirtualPath)
-						if err != nil {
-							return nil, ctx, err
-						}
-						if matchesRealPath || matchesVirtualPath {
-							packages = append(packages[:i], packages[i+1:]...)
-							i--
-						}
-					}
-				}
+			packages, err = filterPackageExclusions(packages, exclusions)
+			if err != nil {
+				return nil, ctx, err
 			}
 		}
 		return packages, ctx, err
 	}
 
 	return syftProvider(userInput, scopeOpt, registryOptions, exclusions...)
+}
+
+func filterPackageExclusions(packages []Package, exclusions []string) ([]Package, error) {
+	for i := 0; i < len(packages); i++ {
+		pkg := packages[i]
+		for _, exclusion := range exclusions {
+			for _, location := range pkg.Locations {
+				matches, err := matchesLocation(exclusion, location)
+				if err != nil {
+					return nil, err
+				}
+				if matches {
+					packages = append(packages[:i], packages[i+1:]...)
+					i--
+				}
+			}
+		}
+	}
+	return packages, nil
+}
+
+func matchesLocation(exclusion string, location source.Location) (bool, error) {
+	matchesRealPath, err := doublestar.Match(exclusion, location.RealPath)
+	if err != nil {
+		return false, err
+	}
+	matchesVirtualPath, err := doublestar.Match(exclusion, location.VirtualPath)
+	if err != nil {
+		return false, err
+	}
+	return matchesRealPath || matchesVirtualPath, nil
 }
