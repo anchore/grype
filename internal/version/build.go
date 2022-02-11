5aa85338d68ceff8c24f713731e94fc1bfe7dd81
Alex Goodman <wagoodman@users.noreply.github.com>
2022-02-11 14:24:25 -0500

    Normalize release assets and refactor install.sh (#630)
    * refactor release to keep snapshot assets in parity with release assets
    
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>
    
    * refactor install.sh and put under test
    
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>
    
    * tidy go.sum
    
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>
    
    * add mac acceptance test to github actions workflow
    
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>
    
    * rm use of goreleaser in cli tests
    
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>
    
    * go mod tidy with go 1.17
    
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>

--- internal/version/build.go
+++ internal/version/build.go
@@ -3,37 +3,48 @@ package version
 import (
 	"fmt"
 	"runtime"
+	"strings"
 )
 
 const valueNotProvided = "[not provided]"
 
+// all variables here are provided as build-time arguments, with clear default values
 var version = valueNotProvided
 var syftVersion = valueNotProvided
 var gitCommit = valueNotProvided
-var gitTreeState = valueNotProvided
+var gitDescription = valueNotProvided
 var buildDate = valueNotProvided
 var platform = fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH)
 
+// Version defines the application version details (generally from build information)
 type Version struct {
-	Version      string `json:"version"`
-	SyftVersion  string `json:"syftVersion"`
-	GitCommit    string `json:"gitCommit"`
-	GitTreeState string `json:"gitTreeState"`
-	BuildDate    string `json:"buildDate"`
-	GoVersion    string `json:"goVersion"`
-	Compiler     string `json:"compiler"`
-	Platform     string `json:"platform"`
+	Version        string `json:"version"`        // application semantic version
+	SyftVersion    string `json:"syftVersion"`    // the version of syft being used by grype
+	GitCommit      string `json:"gitCommit"`      // git SHA at build-time
+	GitDescription string `json:"gitDescription"` // output of 'git describe --dirty --always --tags'
+	BuildDate      string `json:"buildDate"`      // date of the build
+	GoVersion      string `json:"goVersion"`      // go runtime version at build-time
+	Compiler       string `json:"compiler"`       // compiler used at build-time
+	Platform       string `json:"platform"`       // GOOS and GOARCH at build-time
 }
 
+func (v Version) isProductionBuild() bool {
+	if strings.Contains(v.Version, "SNAPSHOT") || strings.Contains(v.Version, valueNotProvided) {
+		return false
+	}
+	return true
+}
+
+// FromBuild provides all version details
 func FromBuild() Version {
 	return Version{
-		Version:      version,
-		SyftVersion:  syftVersion,
-		GitCommit:    gitCommit,
-		GitTreeState: gitTreeState,
-		BuildDate:    buildDate,
-		GoVersion:    runtime.Version(),
-		Compiler:     runtime.Compiler,
-		Platform:     platform,
+		Version:        version,
+		SyftVersion:    syftVersion,
+		GitCommit:      gitCommit,
+		GitDescription: gitDescription,
+		BuildDate:      buildDate,
+		GoVersion:      runtime.Version(),
+		Compiler:       runtime.Compiler,
+		Platform:       platform,
 	}
 }
---
+++ test/install/2_download_release_asset_test.sh
@@ -0,0 +1,41 @@
+. test_harness.sh
+
+test_download_release_asset() {
+  release="$1"
+  os="$2"
+  arch="$3"
+  format="$4"
+  expected_mime_type="$5"
+
+  # for troubleshooting
+  # log_set_priority 10
+
+  name=${PROJECT_NAME}
+  version=$(tag_to_version ${release})
+  github_download="https://github.com/${OWNER}/${REPO}/releases/download/${release}"
+
+  tmpdir=$(mktemp -d)
+
+  actual_filepath=$(download_asset "${github_download}" "${tmpdir}" "${name}" "${os}" "${arch}" "${version}" "${format}" )
+
+  assertFileExists "${actual_filepath}" "download_asset os=${os} arch=${arch} format=${format}"
+
+  actual_mime_type=$(file -b --mime-type ${actual_filepath})
+
+  assertEquals "${expected_mime_type}" "${actual_mime_type}" "unexpected mimetype for os=${os} arch=${arch} format=${format}"
+
+  rm -rf -- "$tmpdir"
+}
+
+# always test against the latest release
+release=$(get_release_tag "${OWNER}" "${REPO}" "latest" )
+
+# exercise all possible assets against a real github release (based on asset listing from https://github.com/anchore/grype/releases/tag/v0.32.0)
+run_test_case test_download_release_asset "${release}" "darwin" "amd64" "tar.gz" "application/gzip"
+run_test_case test_download_release_asset "${release}" "darwin" "arm64" "tar.gz" "application/gzip"
+run_test_case test_download_release_asset "${release}" "linux" "amd64" "tar.gz" "application/gzip"
+run_test_case test_download_release_asset "${release}" "linux" "amd64" "rpm" "application/x-rpm"
+run_test_case test_download_release_asset "${release}" "linux" "amd64" "deb" "application/vnd.debian.binary-package"
+run_test_case test_download_release_asset "${release}" "linux" "arm64" "tar.gz" "application/gzip"
+run_test_case test_download_release_asset "${release}" "linux" "arm64" "rpm" "application/x-rpm"
+run_test_case test_download_release_asset "${release}" "linux" "arm64" "deb" "application/vnd.debian.binary-package"
