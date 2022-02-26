16cd14519a691e769a729c56a1f2cf8faed6427a
Alex Goodman <wagoodman@users.noreply.github.com>
2022-02-26 17:28:08 -0500

    Bump syft to release version v0.39.0 (#645)
    * bump syft to v0.39.0
    
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>
    
    * update ByCriteria to log error on failure
    
    Signed-off-by: Christopher Phillips <christopher.phillips@anchore.com>
    
    * integration tests now pass
    
    Signed-off-by: Christopher Phillips <christopher.phillips@anchore.com>
    
    * bump to v0.39.3
    
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>
    
    * raise search failures to warn
    
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>
    
    * tidy go.mod/sum
    
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>
    
    Co-authored-by: Christopher Phillips <christopher.phillips@anchore.com>

--- grype/search/criteria.go
+++ grype/search/criteria.go
@@ -5,6 +5,7 @@ import (
 	"github.com/anchore/grype/grype/match"
 	"github.com/anchore/grype/grype/pkg"
 	"github.com/anchore/grype/grype/vulnerability"
+	"github.com/anchore/grype/internal/log"
 )
 
 var (
@@ -20,25 +21,28 @@ var (
 type Criteria string
 
 func ByCriteria(store vulnerability.Provider, d *distro.Distro, p pkg.Package, upstreamMatcher match.MatcherType, criteria ...Criteria) ([]match.Match, error) {
-	var matches []match.Match
+	matches := make([]match.Match, 0)
 	for _, c := range criteria {
 		switch c {
 		case ByCPE:
 			m, err := ByPackageCPE(store, p, upstreamMatcher)
 			if err != nil {
-				return nil, err
+				log.Warnf("could not match by package CPE (package=%+v): %v", p, err)
+				continue
 			}
 			matches = append(matches, m...)
 		case ByLanguage:
 			m, err := ByPackageLanguage(store, p, upstreamMatcher)
 			if err != nil {
-				return nil, err
+				log.Warnf("could not match by package language (package=%+v): %v", p, err)
+				continue
 			}
 			matches = append(matches, m...)
 		case ByDistro:
 			m, err := ByPackageDistro(store, d, p, upstreamMatcher)
 			if err != nil {
-				return nil, err
+				log.Warnf("could not match by package distro (package=%+v): %v", p, err)
+				continue
 			}
 			matches = append(matches, m...)
 		}
