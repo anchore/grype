c7d49fd08e65baa4898096bb4d1f94b0eb6e9bce
Keith Zantow <kzantow@gmail.com>
2022-03-03 14:08:43 -0500

    add some basic tests and lint
    Signed-off-by: Keith Zantow <kzantow@gmail.com>

--- grype/presenter/sarif/presenter.go
+++ grype/presenter/sarif/presenter.go
@@ -2,6 +2,7 @@ package sarif
 
 import (
 	"fmt"
+	"github.com/anchore/grype/internal/log"
 	"io"
 	"strings"
 
@@ -33,45 +34,63 @@ func NewPresenter(results match.Matches, packages []pkg.Package, srcMetadata *so
 	}
 }
 
-// Present creates a SARIF-based reporting
+// Present creates a SARIF-based report
 func (pres *Presenter) Present(output io.Writer) error {
 	doc, err := s.New(s.Version210)
+	if err != nil {
+		return err
+	}
+
 	doc.AddRun(&s.Run{
 		Tool: s.Tool{
 			Driver: &s.ToolComponent{
 				Name:    "Anchore Container Vulnerability Report (T0)",
 				Version: sp(version.FromBuild().Version),
-				Rules:   pres.toSarifRules(),
+				Rules:   pres.sarifRules(),
 			},
 		},
-		Results: pres.toSarifResults(),
+		Results: pres.sarifResults(),
 	})
 
 	err = doc.PrettyWrite(output)
 	return err
 }
 
-func (pres *Presenter) toSarifRules() (out []*s.ReportingDescriptor) {
+// matches returns a set of matches with duplicates removed
+func (pres *Presenter) matches() (out []match.Match) {
 	ruleIDs := map[string]bool{}
+
+	for _, m := range pres.results.Sorted() {
+		ruleID := pres.ruleID(m)
+
+		if ruleIDs[ruleID] {
+			log.Infof("skipping duplicate match: %s", m.String())
+			continue
+		}
+
+		ruleIDs[ruleID] = true
+
+		out = append(out, m)
+	}
+
+	return out
+}
+
+// sarifRules generates the set of rules to include in this run
+func (pres *Presenter) sarifRules() (out []*s.ReportingDescriptor) {
 	if pres.results.Count() > 0 {
-		for _, v := range pres.results.Sorted() {
-			ruleID := pres.ruleID(v)
-
-			if ruleIDs[ruleID] {
-				continue
-			}
-
-			ruleIDs[ruleID] = true
+		for _, m := range pres.matches() {
+			ruleID := pres.ruleID(m)
 
 			// Entirely possible to not have any links whatsoever
-			link := v.Vulnerability.ID
-			md, err := pres.metadataProvider.GetMetadata(v.Vulnerability.ID, v.Vulnerability.Namespace)
-			if err != nil {
+			link := m.Vulnerability.ID
+			meta := pres.metadata(m)
+			if meta != nil {
 				switch {
-				case md.DataSource != "":
-					link = fmt.Sprintf("[%s](%s)", v.Vulnerability.ID, md.DataSource)
-				case len(md.URLs) > 0:
-					link = fmt.Sprintf("[%s](%s)", v.Vulnerability.ID, md.URLs[0])
+				case meta.DataSource != "":
+					link = fmt.Sprintf("[%s](%s)", m.Vulnerability.ID, meta.DataSource)
+				case len(meta.URLs) > 0:
+					link = fmt.Sprintf("[%s](%s)", m.Vulnerability.ID, meta.URLs[0])
 				}
 			}
 
@@ -79,37 +98,37 @@ func (pres *Presenter) toSarifRules() (out []*s.ReportingDescriptor) {
 				ID: ruleID,
 				// Title of the SARIF report
 				ShortDescription: &s.MultiformatMessageString{
-					Text: sp(pres.shortDescription(v)),
+					Text: sp(pres.shortDescription(m)),
 				},
 				// Subtitle of the SARIF report
 				FullDescription: &s.MultiformatMessageString{
-					Text: sp(subtitle(v)),
+					Text: sp(pres.subtitle(m)),
 				},
-				Help: pres.helpText(v, link),
+				Help: pres.helpText(m, link),
 			})
 		}
 	}
-	return
+	return out
 }
 
-func (pres *Presenter) ruleID(v match.Match) string {
-	ruleID := fmt.Sprintf("ANCHOREVULN_%s_%s_%s_%s", v.Vulnerability.ID, v.Package.Type, v.Package.Name, v.Package.Version)
+func (pres *Presenter) ruleID(m match.Match) string {
+	ruleID := fmt.Sprintf("ANCHOREVULN_%s_%s_%s_%s", m.Vulnerability.ID, m.Package.Type, m.Package.Name, m.Package.Version)
 	if pres.srcMetadata.Scheme == source.ImageScheme {
 		// include the container as part of the rule id so that users can sort by that
-		ruleID = fmt.Sprintf("ANCHOREVULN_%s_%s_%s_%s_%s", pres.srcMetadata.ImageMetadata.UserInput, v.Vulnerability.ID, v.Package.Type, v.Package.Name, v.Package.Version)
+		ruleID = fmt.Sprintf("ANCHOREVULN_%s_%s_%s_%s_%s", pres.srcMetadata.ImageMetadata.UserInput, m.Vulnerability.ID, m.Package.Type, m.Package.Name, m.Package.Version)
 	}
 	return ruleID
 }
 
-func (pres *Presenter) helpText(v match.Match, link string) *s.MultiformatMessageString {
+func (pres *Presenter) helpText(m match.Match, link string) *s.MultiformatMessageString {
 	text := fmt.Sprintf("Vulnerability %s\nSeverity: %s\nPackage: %s\nVersion: %s\nFix Version: %s\nType: %s\nLocation: %s\nData Namespace: unknown\nLink: %s",
-		v.Vulnerability.ID, pres.severity(v), v.Package.Name, v.Package.Version, fixVersions(v), v.Package.Type, v.Package.Locations[0].VirtualPath, link,
+		m.Vulnerability.ID, pres.severity(m), m.Package.Name, m.Package.Version, fixVersions(m), m.Package.Type, location(m), link,
 	)
 	markdown := fmt.Sprintf(
 		"**Vulnerability %s**\n"+
 			"| Severity | Package | Version | Fix Version | Type | Location | Data Namespace | Link |\n"+
 			"| --- | --- | --- | --- | --- | --- | --- | --- |\n"+
-			"|%s|%s|%s|%s|%s|%s|unknown|%s|\n", v.Vulnerability.ID, pres.severity(v), v.Package.Name, v.Package.Version, fixVersions(v), v.Package.Type, v.Package.Locations[0].VirtualPath, link,
+			"|%s|%s|%s|%s|%s|%s|unknown|%s|\n", m.Vulnerability.ID, pres.severity(m), m.Package.Name, m.Package.Version, fixVersions(m), m.Package.Type, location(m), link,
 	)
 	return &s.MultiformatMessageString{
 		Text:     &text,
@@ -117,54 +136,74 @@ func (pres *Presenter) helpText(v match.Match, link string) *s.MultiformatMessag
 	}
 }
 
-func (pres *Presenter) severity(v match.Match) string {
-	md, err := pres.metadataProvider.GetMetadata(v.Vulnerability.ID, v.Vulnerability.Namespace)
-	if err != nil {
+func location(m match.Match) string {
+	if len(m.Package.Locations) > 0 {
+		return m.Package.Locations[0].VirtualPath
+	}
+	// XXX there is room for improvement here, trying to mimick previous behavior
+	// If no `dockerfile-path` was provided, and in the improbable situation where there
+	// are no locations for the artifact, return 'Dockerfile'
+	return "Dockerfile"
+}
+
+func (pres *Presenter) severity(m match.Match) string {
+	meta := pres.metadata(m)
+	if meta == nil {
 		return "unknown"
 	}
 	// FIXME allow severity cutoff specified here?
 	// FIXME convert to acs_
-	return md.Severity
+	return meta.Severity
 }
 
-func subtitle(v match.Match) string {
-	subtitle := v.Vulnerability.String()
-	if subtitle != "" {
-		return subtitle
-	}
-
-	subtitle = fixVersions(v)
-	if subtitle != "" {
-		return fmt.Sprintf("Version %s is affected with an available fix in versions %s", v.Package.Version, subtitle)
-	}
-
-	return fmt.Sprintf("Version %s is affected with no fixes reported yet.", v.Package.Version)
+// metadata returns the matching *vulnerability.Metadata from the provider or nil if not found / error
+func (pres *Presenter) metadata(m match.Match) *vulnerability.Metadata {
+	meta, _ := pres.metadataProvider.GetMetadata(m.Vulnerability.ID, m.Vulnerability.Namespace)
+	return meta
 }
 
-func fixVersions(v match.Match) string {
-	if v.Vulnerability.Fix.State == v3.FixedState && len(v.Vulnerability.Fix.Versions) > 0 {
-		return strings.Join(v.Vulnerability.Fix.Versions, ",")
+// subtitle generates a subtitle for the given match
+func (pres *Presenter) subtitle(m match.Match) string {
+	meta := pres.metadata(m)
+	if meta != nil {
+		subtitle := meta.Description
+		if subtitle != "" {
+			return subtitle
+		}
+	}
+
+	fixVersion := fixVersions(m)
+	if fixVersion != "" {
+		return fmt.Sprintf("Version %s is affected with an available fix in versions %s", m.Package.Version, fixVersion)
+	}
+
+	return fmt.Sprintf("Version %s is affected with no fixes reported yet.", m.Package.Version)
+}
+
+func fixVersions(m match.Match) string {
+	if m.Vulnerability.Fix.State == v3.FixedState && len(m.Vulnerability.Fix.Versions) > 0 {
+		return strings.Join(m.Vulnerability.Fix.Versions, ",")
 	}
 	return ""
 }
 
-func (pres *Presenter) shortDescription(v match.Match) string {
-	return fmt.Sprintf("%s %s vulnerability for %s package", v.Vulnerability.ID, pres.severity(v), v.Package.Name)
+func (pres *Presenter) shortDescription(m match.Match) string {
+	return fmt.Sprintf("%s %s vulnerability for %s package", m.Vulnerability.ID, pres.severity(m), m.Package.Name)
 }
 
-func (pres *Presenter) toSarifResults() (out []*s.Result) {
-	for _, v := range pres.results.Sorted() {
+func (pres *Presenter) sarifResults() (out []*s.Result) {
+	for _, m := range pres.matches() {
 		out = append(out, &s.Result{
-			RuleID:         sp(pres.ruleID(v)),
+			RuleID:         sp(pres.ruleID(m)),
 			RuleIndex:      up(0),
-			Level:          sp(pres.acsSeverityLevel(v)),
-			Message:        pres.resultMessage(v),
-			AnalysisTarget: pres.analysisTarget(v),
+			Level:          sp(pres.acsSeverityLevel(m)),
+			Message:        pres.resultMessage(m),
+			AnalysisTarget: pres.analysisTarget(m),
 			Locations: []*s.Location{
 				{
 					PhysicalLocation: &s.PhysicalLocation{
 						ArtifactLocation: &s.ArtifactLocation{
-							URI: sp(getLocation(v)),
+							URI: sp(location(m)),
 						},
 						// TODO: When grype starts reporting line numbers this will need to get updated
 						Region: &s.Region{
@@ -191,30 +230,33 @@ func (pres *Presenter) toSarifResults() (out []*s.Result) {
 			BaselineState: sp("unchanged"),
 		})
 	}
-	return
+	return out
 }
 
+// up returns a uint pointer based on the provided value
 func up(i int) *uint {
 	u := uint(i)
 	return &u
 }
 
+// ip returns an int pointer based on the provided value
 func ip(i int) *int {
 	return &i
 }
 
+// sp returns a string pointer based on the provided value
 func sp(s string) *string {
 	return &s
 }
 
-func (pres *Presenter) resultMessage(v match.Match) s.Message {
-	path := getLocation(v)
-	message := fmt.Sprintf("The path %s reports %s at version %s ", path, v.Package.Name, v.Package.Version)
+func (pres *Presenter) resultMessage(m match.Match) s.Message {
+	path := location(m)
+	message := fmt.Sprintf("The path %s reports %s at version %s ", path, m.Package.Name, m.Package.Version)
 
 	if pres.srcMetadata.Scheme == source.DirectoryScheme {
-		message = fmt.Sprintf("%s which would result in a vulnerable (%s) package installed", message, v.Package.Type)
+		message = fmt.Sprintf("%s which would result in a vulnerable (%s) package installed", message, m.Package.Type)
 	} else {
-		message = fmt.Sprintf("%s which is a vulnerable (%s) package installed in the container", message, v.Package.Type)
+		message = fmt.Sprintf("%s which is a vulnerable (%s) package installed in the container", message, m.Package.Type)
 	}
 
 	return s.Message{
@@ -223,30 +265,17 @@ func (pres *Presenter) resultMessage(v match.Match) s.Message {
 	}
 }
 
-func getLocation(v match.Match) string {
-	if len(v.Package.Locations) > 0 {
-		// If the scan was against a directory, the location will be a string
-		location := v.Package.Locations[0]
-		return location.VirtualPath
-	}
-	// XXX there is room for improvement here, trying to mimick previous behavior
-	// If no `dockerfile-path` was provided, and in the improbable situation where there
-	// are no locations for the artifact, return 'Dockerfile'
-	return "Dockerfile"
-
-}
-
-func (pres *Presenter) analysisTarget(v match.Match) *s.ArtifactLocation {
-	uri := getLocation(v)
+func (pres *Presenter) analysisTarget(m match.Match) *s.ArtifactLocation {
+	uri := location(m)
 	return &s.ArtifactLocation{
 		URI: &uri,
 		// XXX This is possibly a bug. The SARIF schema invalidates this when the index is present because there
 		// aren't any other elements present.
-		//"index": 0
+		// Index: up(0),
 	}
 }
 
-func (pres *Presenter) acsSeverityLevel(v match.Match) string {
+func (pres *Presenter) acsSeverityLevel(m match.Match) string {
 	// FIXME need an input cutoff param
 	// The `severity_cutoff_param` has been lowercased for case-insensitivity at this point, but the
 	// severity from the vulnerability will be capitalized, so this must be capitalized again to calculate
@@ -262,7 +291,7 @@ func (pres *Presenter) acsSeverityLevel(v match.Match) string {
 		"Critical":   5,
 	}
 
-	if severityLevels[pres.severity(v)] < severityLevels[cutoff] {
+	if severityLevels[pres.severity(m)] < severityLevels[cutoff] {
 		out = "warning"
 	}
 
---
+++ grype/presenter/sarif/presenter_test.go
@@ -0,0 +1,162 @@
+package sarif
+
+import (
+	"bytes"
+	"flag"
+	"regexp"
+	"testing"
+
+	"github.com/anchore/go-testutils"
+	"github.com/anchore/grype/grype/match"
+	"github.com/anchore/grype/grype/pkg"
+	"github.com/anchore/grype/grype/presenter/models"
+	"github.com/anchore/grype/grype/vulnerability"
+	"github.com/anchore/stereoscope/pkg/imagetest"
+	syftPkg "github.com/anchore/syft/syft/pkg"
+	"github.com/anchore/syft/syft/source"
+	"github.com/sergi/go-diff/diffmatchpatch"
+)
+
+var update = flag.Bool("update", false, "update the *.golden files for json presenters")
+
+func createResults() (match.Matches, []pkg.Package) {
+
+	pkg1 := pkg.Package{
+		ID:      "package-1-id",
+		Name:    "package-1",
+		Version: "1.0.1",
+		Type:    syftPkg.DebPkg,
+	}
+	pkg2 := pkg.Package{
+		ID:      "package-2-id",
+		Name:    "package-2",
+		Version: "2.0.1",
+		Type:    syftPkg.DebPkg,
+		Licenses: []string{
+			"MIT",
+			"Apache-v2",
+		},
+	}
+
+	var match1 = match.Match{
+
+		Vulnerability: vulnerability.Vulnerability{
+			ID:        "CVE-1999-0001",
+			Namespace: "source-1",
+		},
+		Package: pkg1,
+		Details: []match.Detail{
+			{
+				Type:    match.ExactDirectMatch,
+				Matcher: match.DpkgMatcher,
+			},
+		},
+	}
+
+	var match2 = match.Match{
+
+		Vulnerability: vulnerability.Vulnerability{
+			ID:        "CVE-1999-0002",
+			Namespace: "source-2",
+		},
+		Package: pkg2,
+		Details: []match.Detail{
+			{
+				Type:    match.ExactIndirectMatch,
+				Matcher: match.DpkgMatcher,
+				SearchedBy: map[string]interface{}{
+					"some": "key",
+				},
+			},
+		},
+	}
+
+	matches := match.NewMatches()
+
+	matches.Add(match1, match2)
+
+	return matches, []pkg.Package{pkg1, pkg2}
+}
+
+func TestSarifPresenterImage(t *testing.T) {
+	var buffer bytes.Buffer
+
+	matches, packages := createResults()
+
+	img := imagetest.GetFixtureImage(t, "docker-archive", "image-simple")
+	s, err := source.NewFromImage(img, "user-input")
+
+	// This accounts for the non-deterministic digest value that we end up with when
+	// we build a container image dynamically during testing. Ultimately, we should
+	// use a golden image as a test fixture in place of building this image during
+	// testing. At that time, this line will no longer be necessary.
+	//
+	// This value is sourced from the "version" node in "./test-fixtures/snapshot/TestSarifImgsPresenter.golden"
+	s.Metadata.ImageMetadata.ManifestDigest = "sha256:2731251dc34951c0e50fcc643b4c5f74922dad1a5d98f302b504cf46cd5d9368"
+
+	pres := NewPresenter(matches, packages, &s.Metadata, models.NewMetadataMock())
+	// run presenter
+	err = pres.Present(&buffer)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	actual := buffer.Bytes()
+	if *update {
+		testutils.UpdateGoldenFileContents(t, actual)
+	}
+
+	var expected = testutils.GetGoldenFileContents(t)
+
+	// remove dynamic values, which are tested independently
+	actual = redact(actual)
+	expected = redact(expected)
+
+	if !bytes.Equal(expected, actual) {
+		dmp := diffmatchpatch.New()
+		diffs := dmp.DiffMain(string(expected), string(actual), true)
+		t.Errorf("mismatched output:\n%s", dmp.DiffPrettyText(diffs))
+	}
+}
+
+func TestSarifPresenterDir(t *testing.T) {
+	var buffer bytes.Buffer
+	matches, packages := createResults()
+
+	s, err := source.NewFromDirectory("/some/path")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	pres := NewPresenter(matches, packages, &s.Metadata, models.NewMetadataMock())
+
+	// run presenter
+	err = pres.Present(&buffer)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	actual := buffer.Bytes()
+	if *update {
+		testutils.UpdateGoldenFileContents(t, actual)
+	}
+
+	var expected = testutils.GetGoldenFileContents(t)
+
+	// remove dynamic values, which are tested independently
+	actual = redact(actual)
+	expected = redact(expected)
+
+	if !bytes.Equal(expected, actual) {
+		dmp := diffmatchpatch.New()
+		diffs := dmp.DiffMain(string(expected), string(actual), true)
+		t.Errorf("mismatched output:\n%s", dmp.DiffPrettyText(diffs))
+	}
+}
+
+func redact(s []byte) []byte {
+	for _, pattern := range []*regexp.Regexp{} {
+		s = pattern.ReplaceAll(s, []byte("redacted"))
+	}
+	return s
+}
