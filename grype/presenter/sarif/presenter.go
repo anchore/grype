41ab74546548f72b6a6863287cb420af2cd71e62
Keith Zantow <kzantow@gmail.com>
2022-03-07 21:21:14 -0500

    try more things with github
    Signed-off-by: Keith Zantow <kzantow@gmail.com>

--- grype/presenter/sarif/presenter.go
+++ grype/presenter/sarif/presenter.go
@@ -182,7 +182,6 @@ func (pres *Presenter) location(m match.Match) string {
 func (pres *Presenter) locations(m match.Match) []*s.Location {
 	var logicalLocations []*s.LogicalLocation
 	physicalLocation := pres.location(m)
-	message := physicalLocation
 
 	switch pres.srcMetadata.Scheme {
 	case source.ImageScheme:
@@ -201,7 +200,6 @@ func (pres *Presenter) locations(m match.Match) []*s.Location {
 		// TODO we could add configuration to specify the prefix, a user might want to specify an image name and architecture
 		// in the case of multiple vuln scans, for example
 		physicalLocation = fmt.Sprintf("image/%s", strings.TrimPrefix(physicalLocation, "/"))
-		message = fmt.Sprintf("image://%s/%s", img, physicalLocation)
 	case source.FileScheme:
 		for _, l := range m.Package.Locations {
 			logicalLocations = append(logicalLocations, &s.LogicalLocation{
@@ -210,15 +208,11 @@ func (pres *Presenter) locations(m match.Match) []*s.Location {
 			})
 		}
 
-		message = fmt.Sprintf("image://%s/%s", pres.srcMetadata.Path, physicalLocation)
 		physicalLocation = pres.srcMetadata.Path
 	}
 
 	return []*s.Location{
 		{
-			Message: &s.Message{
-				Text: sp(fmt.Sprintf("MESSAGE: %s", message)),
-			},
 			PhysicalLocation: &s.PhysicalLocation{
 				ArtifactLocation: &s.ArtifactLocation{
 					URI: sp(physicalLocation),
