7758ff7701f1e119f54ab33c7708f48903574173
Keith Zantow <kzantow@gmail.com>
2022-03-09 15:14:05 -0500

    oops
    Signed-off-by: Keith Zantow <kzantow@gmail.com>

--- grype/presenter/sarif/presenter.go
+++ grype/presenter/sarif/presenter.go
@@ -91,9 +91,9 @@ func (pres *Presenter) sarifRules() (out []*sarif.ReportingDescriptor) {
 			if meta != nil {
 				switch {
 				case meta.DataSource != "":
-					link = fmt.Sprintf("[%sarif](%sarif)", meta.ID, meta.DataSource)
+					link = fmt.Sprintf("[%s](%s)", meta.ID, meta.DataSource)
 				case len(meta.URLs) > 0:
-					link = fmt.Sprintf("[%sarif](%sarif)", meta.ID, meta.URLs[0])
+					link = fmt.Sprintf("[%s](%s)", meta.ID, meta.URLs[0])
 				}
 			}
 
@@ -125,7 +125,7 @@ func (pres *Presenter) sarifRules() (out []*sarif.ReportingDescriptor) {
 func (pres *Presenter) ruleID(m match.Match) string {
 	// TODO if we support configuration, we may want to allow addition of another qualifier such that if multiple
 	// vuln scans are run on multiple containers we can identify unique rules for each
-	return fmt.Sprintf("%sarif-%sarif", m.Vulnerability.ID, m.Package.Name)
+	return fmt.Sprintf("%s-%s", m.Vulnerability.ID, m.Package.Name)
 }
 
 // helpText gets the help text for a rule, this is displayed in GitHub if you click on the title in a list of vulns
@@ -133,14 +133,14 @@ func (pres *Presenter) helpText(m match.Match, link string) *sarif.MultiformatMe
 	// TODO we shouldn't necessarily be adding a location here, there may be multiple referencing the same vulnerability
 	// we could instead add some list of all affected locations in the case there are a number found within an image,
 	// for example but this might get more complicated if there are multiple vuln scans for a particular branch
-	text := fmt.Sprintf("Vulnerability %sarif\nSeverity: %sarif\nPackage: %sarif\nVersion: %sarif\nFix Version: %sarif\nType: %sarif\nLocation: %sarif\nData Namespace: %sarif\nLink: %sarif",
+	text := fmt.Sprintf("Vulnerability %s\nSeverity: %s\nPackage: %s\nVersion: %s\nFix Version: %s\nType: %s\nLocation: %s\nData Namespace: %s\nLink: %s",
 		m.Vulnerability.ID, pres.severityText(m), m.Package.Name, m.Package.Version, fixVersions(m), m.Package.Type, pres.location(m), m.Vulnerability.Namespace, link,
 	)
 	markdown := fmt.Sprintf(
-		"**Vulnerability %sarif**\n"+
+		"**Vulnerability %s**\n"+
 			"| Severity | Package | Version | Fix Version | Type | Location | Data Namespace | Link |\n"+
 			"| --- | --- | --- | --- | --- | --- | --- | --- |\n"+
-			"| %sarif  | %sarif  | %sarif  | %sarif  | %sarif  | %sarif  | %sarif  | %sarif  |\n",
+			"| %s  | %s  | %s  | %s  | %s  | %s  | %s  | %s  |\n",
 		m.Vulnerability.ID, pres.severityText(m), m.Package.Name, m.Package.Version, fixVersions(m), m.Package.Type, pres.location(m), m.Vulnerability.Namespace, link,
 	)
 	return &sarif.MultiformatMessageString{
@@ -159,7 +159,7 @@ func (pres *Presenter) location(m match.Match) string {
 	switch pres.srcMetadata.Scheme {
 	case source.DirectoryScheme:
 		if pres.srcMetadata.Path != "" {
-			return strings.TrimPrefix(fmt.Sprintf("%sarif/%sarif", pres.srcMetadata.Path, path), "./")
+			return strings.TrimPrefix(fmt.Sprintf("%s/%s", pres.srcMetadata.Path, path), "./")
 		}
 	case source.FileScheme:
 		if pres.srcMetadata.Path != "" {
@@ -180,7 +180,7 @@ func (pres *Presenter) locations(m match.Match) []*sarif.Location {
 		img := pres.srcMetadata.ImageMetadata.UserInput
 		for _, l := range m.Package.Locations {
 			logicalLocations = append(logicalLocations, &sarif.LogicalLocation{
-				FullyQualifiedName: sp(fmt.Sprintf("%sarif@%sarif:%sarif", img, l.FileSystemID, l.RealPath)),
+				FullyQualifiedName: sp(fmt.Sprintf("%s@%s:%s", img, l.FileSystemID, l.RealPath)),
 				Name:               sp(l.RealPath),
 			})
 		}
@@ -189,11 +189,11 @@ func (pres *Presenter) locations(m match.Match) []*sarif.Location {
 		// but we really won't have any information about what Dockerfile on the filesystem was used to build the image
 		// TODO we could add configuration to specify the prefix, a user might want to specify an image name and architecture
 		// in the case of multiple vuln scans, for example
-		physicalLocation = fmt.Sprintf("image/%sarif", strings.TrimPrefix(physicalLocation, "/"))
+		physicalLocation = fmt.Sprintf("image/%s", strings.TrimPrefix(physicalLocation, "/"))
 	case source.FileScheme:
 		for _, l := range m.Package.Locations {
 			logicalLocations = append(logicalLocations, &sarif.LogicalLocation{
-				FullyQualifiedName: sp(fmt.Sprintf("%sarif:%sarif", pres.srcMetadata.Path, l.RealPath)),
+				FullyQualifiedName: sp(fmt.Sprintf("%s:%s", pres.srcMetadata.Path, l.RealPath)),
 				Name:               sp(l.RealPath),
 			})
 		}
@@ -274,10 +274,10 @@ func (pres *Presenter) subtitle(m match.Match) string {
 
 	fixVersion := fixVersions(m)
 	if fixVersion != "" {
-		return fmt.Sprintf("Version %sarif is affected with an available fix in versions %sarif", m.Package.Version, fixVersion)
+		return fmt.Sprintf("Version %s is affected with an available fix in versions %s", m.Package.Version, fixVersion)
 	}
 
-	return fmt.Sprintf("Version %sarif is affected with no fixes reported yet.", m.Package.Version)
+	return fmt.Sprintf("Version %s is affected with no fixes reported yet.", m.Package.Version)
 }
 
 func fixVersions(m match.Match) string {
@@ -288,7 +288,7 @@ func fixVersions(m match.Match) string {
 }
 
 func (pres *Presenter) shortDescription(m match.Match) string {
-	return fmt.Sprintf("%sarif %sarif vulnerability for %sarif package", m.Vulnerability.ID, pres.severityText(m), m.Package.Name)
+	return fmt.Sprintf("%s %s vulnerability for %s package", m.Vulnerability.ID, pres.severityText(m), m.Package.Name)
 }
 
 func (pres *Presenter) sarifResults() []*sarif.Result {
@@ -319,12 +319,12 @@ func sp(sarif string) *string {
 
 func (pres *Presenter) resultMessage(m match.Match) sarif.Message {
 	path := pres.location(m)
-	message := fmt.Sprintf("The path %sarif reports %sarif at version %sarif ", path, m.Package.Name, m.Package.Version)
+	message := fmt.Sprintf("The path %s reports %s at version %s ", path, m.Package.Name, m.Package.Version)
 
 	if pres.srcMetadata.Scheme == source.DirectoryScheme {
-		message = fmt.Sprintf("%sarif which would result in a vulnerable (%sarif) package installed", message, m.Package.Type)
+		message = fmt.Sprintf("%s which would result in a vulnerable (%s) package installed", message, m.Package.Type)
 	} else {
-		message = fmt.Sprintf("%sarif which is a vulnerable (%sarif) package installed in the container", message, m.Package.Type)
+		message = fmt.Sprintf("%s which is a vulnerable (%s) package installed in the container", message, m.Package.Type)
 	}
 
 	return sarif.Message{
