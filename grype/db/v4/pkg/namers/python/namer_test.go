cdcfdf7107ee0fa308e036d8b2b2a9ca3dcc8cc7
Weston Steimel <weston.steimel@anchore.com>
2022-05-30 15:04:31 +0100

    adding normalization of package names at database adapter layer
    Signed-off-by: Weston Steimel <weston.steimel@anchore.com>

---
+++ grype/db/v4/pkg/namers/python/namer_test.go
@@ -0,0 +1,41 @@
+package python
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_Normalize(t *testing.T) {
+	tests := []struct {
+		packageName    string
+		normalizedName string
+	}{
+		{
+			packageName:    "PyYAML",
+			normalizedName: "pyyaml",
+		},
+		{
+			packageName:    "oslo.concurrency",
+			normalizedName: "oslo-concurrency",
+		},
+		{
+			packageName:    "",
+			normalizedName: "",
+		},
+		{
+			packageName:    "test---1",
+			normalizedName: "test-1",
+		},
+		{
+			packageName:    "AbCd.-__.--.-___.__.--1234____----....XyZZZ",
+			normalizedName: "abcd-1234-xyzzz",
+		},
+	}
+
+	namer := Namer{}
+
+	for _, test := range tests {
+		normalizedName, _ := namer.Normalize(test.packageName)
+		assert.Equal(t, normalizedName, test.normalizedName)
+	}
+}
