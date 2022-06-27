de270b28c81fc6f809c53ea28c897280ce1412e8
cpendery <cpendery@vt.edu>
2022-06-27 19:20:57 -0400

    test: only_vulnerable_targets & format
    Signed-off-by: cpendery <cpendery@vt.edu>

--- grype/search/only_vulnerable_targets.go
+++ grype/search/only_vulnerable_targets.go
@@ -1,18 +1,19 @@
 package search
 
 import (
+	"github.com/facebookincubator/nvdtools/wfn"
+
 	"github.com/anchore/grype/grype/pkg"
 	"github.com/anchore/grype/grype/vulnerability"
 	syftPkg "github.com/anchore/syft/syft/pkg"
-	"github.com/facebookincubator/nvdtools/wfn"
 )
 
-//Determines if a vulnerability is an accurate match using the vulnerability's cpes' target software
+// Determines if a vulnerability is an accurate match using the vulnerability's cpes' target software
 func onlyVulnerableTargets(p pkg.Package, allVulns []vulnerability.Vulnerability) []vulnerability.Vulnerability {
 	var vulns []vulnerability.Vulnerability
 
 	for _, vuln := range allVulns {
-		isPackageVulnerable := len(vuln.CPEs) != 0
+		isPackageVulnerable := len(vuln.CPEs) == 0
 		for _, cpe := range vuln.CPEs {
 			targetSW := cpe.TargetSW
 			mismatchWithUnknownLanguage := targetSW != string(p.Language) && syftPkg.LanguageByName(targetSW) == syftPkg.UnknownLanguage
