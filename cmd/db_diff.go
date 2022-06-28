73621b178d73f3fd1821f33a8f32989441730933
cpendery <cpendery@vt.edu>
2022-06-28 17:15:23 -0400

    feat: initial implementation work
    Signed-off-by: cpendery <cpendery@vt.edu>

---
+++ cmd/db_diff.go
@@ -0,0 +1,108 @@
+package cmd
+
+import (
+	"fmt"
+	"net/url"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/wagoodman/go-partybus"
+
+	"github.com/anchore/grype/grype/differ"
+	"github.com/anchore/grype/grype/event"
+	"github.com/anchore/grype/internal/bus"
+	"github.com/anchore/grype/internal/log"
+	"github.com/anchore/grype/internal/ui"
+	"github.com/anchore/stereoscope"
+)
+
+var dbDiffOutputFormat string
+
+const deleteFlag string = "delete"
+
+var dbDiffCmd = &cobra.Command{
+	Use:   "diff [flags] base_db_url target_db_url",
+	Short: "diff two DBs and display the result",
+	Args:  cobra.ExactArgs(2),
+	RunE:  runDBDiffCmd,
+}
+
+func init() {
+	dbDiffCmd.Flags().StringVarP(&dbDiffOutputFormat, "output", "o", "table", "format to display results (available=[table, json])")
+	dbDiffCmd.Flags().BoolP(deleteFlag, "d", false, "delete downloaded databases after diff occurs")
+
+	dbCmd.AddCommand(dbDiffCmd)
+}
+
+func startDBDiffCmd(baseUrl, targetUrl string, deleteDatabases bool) <-chan error {
+	errs := make(chan error)
+	go func() {
+		defer close(errs)
+		d, err := differ.NewDiffer(appConfig.DB.ToCuratorConfig())
+		if err != nil {
+			errs <- err
+			return
+		}
+
+		baseUrl, err := url.Parse(baseUrl)
+		if err != nil {
+			errs <- fmt.Errorf("Base url is malformed: %w", err)
+			return
+		}
+		targetUrl, err := url.Parse(targetUrl)
+		if err != nil {
+			errs <- fmt.Errorf("Target url is malformed: %w", err)
+			return
+		}
+
+		if err := d.DownloadDatabases(baseUrl, targetUrl); err != nil {
+			errs <- err
+			return
+		}
+
+		diff, err := d.DiffDatabases()
+		if err != nil {
+			errs <- err
+			return
+		}
+
+		if len(*diff) == 0 {
+			fmt.Println("Databases are identical!")
+		} else {
+			d.Present(dbDiffOutputFormat, diff, os.Stdout)
+		}
+
+		if deleteDatabases {
+			errs <- d.DeleteDatabases()
+		}
+
+		bus.Publish(partybus.Event{
+			Type:  event.NonRootCommandFinished,
+			Value: "",
+		})
+	}()
+	return errs
+}
+
+func runDBDiffCmd(cmd *cobra.Command, args []string) error {
+	reporter, closer, err := reportWriter()
+	defer func() {
+		if err := closer(); err != nil {
+			log.Warnf("unable to write to report destination: %+v", err)
+		}
+	}()
+	if err != nil {
+		return err
+	}
+	deleteDatabases, err := cmd.Flags().GetBool(deleteFlag)
+	if err != nil {
+		return err
+	}
+	return eventLoop(
+		startDBDiffCmd(args[0], args[1], deleteDatabases),
+		setupSignals(),
+		eventSubscription,
+		stereoscope.Cleanup,
+		ui.Select(isVerbose(), appConfig.Quiet, reporter)...,
+	)
+}
---
+++ grype/differ/differ.go
@@ -0,0 +1,185 @@
+package differ
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/url"
+	"os"
+	"path"
+
+	"github.com/olekukonko/tablewriter"
+	"github.com/wagoodman/go-partybus"
+	"github.com/wagoodman/go-progress"
+
+	"github.com/anchore/grype/grype/db"
+	v3 "github.com/anchore/grype/grype/db/v3"
+	"github.com/anchore/grype/grype/event"
+	"github.com/anchore/grype/internal/bus"
+)
+
+type Differ struct {
+	baseCurator   db.Curator
+	targetCurator db.Curator
+}
+
+func NewDiffer(config db.Config) (*Differ, error) {
+	baseCurator, err := db.NewCurator(db.Config{
+		DBRootDir:           path.Join(config.DBRootDir, "diff", "base"),
+		ListingURL:          config.ListingURL,
+		CACert:              config.CACert,
+		ValidateByHashOnGet: config.ValidateByHashOnGet,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	targetCurator, err := db.NewCurator(db.Config{
+		DBRootDir:           path.Join(config.DBRootDir, "diff", "target"),
+		ListingURL:          config.ListingURL,
+		CACert:              config.CACert,
+		ValidateByHashOnGet: config.ValidateByHashOnGet,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &Differ{
+		baseCurator:   baseCurator,
+		targetCurator: targetCurator,
+	}, nil
+}
+
+func (d *Differ) DownloadDatabases(baseUrl, targetUrl *url.URL) error {
+	listing, err := d.baseCurator.ListingFromURL()
+	if err != nil {
+		return err
+	}
+
+	listings := listing.Available
+	dbs := listings[v3.SchemaVersion]
+
+	var baseListing *db.ListingEntry
+	var targetListing *db.ListingEntry
+
+	for _, db := range dbs {
+		if *db.URL == *baseUrl {
+			baseListing = &db
+		}
+		if *db.URL == *targetUrl {
+			targetListing = &db
+		}
+	}
+
+	if baseListing == nil {
+		return fmt.Errorf("Unable to find listing for base url: %s", baseUrl.String())
+	} else if targetListing == nil {
+		return fmt.Errorf("Unable to find listing for target url: %s", targetUrl.String())
+	}
+
+	if err := download(&d.baseCurator, baseListing); err != nil {
+		return fmt.Errorf("unable to update base vulnerability database: %+v", err)
+	}
+	if err := download(&d.targetCurator, targetListing); err != nil {
+		return fmt.Errorf("unable to update target vulnerability database: %+v", err)
+	}
+	return nil
+}
+
+func download(curator *db.Curator, listing *db.ListingEntry) error {
+	// let consumers know of a monitorable event (download + import stages)
+	importProgress := &progress.Manual{
+		Total: 1,
+	}
+	stage := &progress.Stage{
+		Current: "checking available databases",
+	}
+	downloadProgress := &progress.Manual{
+		Total: 1,
+	}
+	aggregateProgress := progress.NewAggregator(progress.DefaultStrategy, downloadProgress, importProgress)
+
+	bus.Publish(partybus.Event{
+		Type: event.UpdateVulnerabilityDatabase,
+		Value: progress.StagedProgressable(&struct {
+			progress.Stager
+			progress.Progressable
+		}{
+			Stager:       progress.Stager(stage),
+			Progressable: progress.Progressable(aggregateProgress),
+		}),
+	})
+
+	defer downloadProgress.SetCompleted()
+	defer importProgress.SetCompleted()
+
+	return curator.UpdateTo(listing, downloadProgress, importProgress, stage)
+}
+
+func (d *Differ) DiffDatabases() (*[]v3.Diff, error) {
+	baseStore, err := d.baseCurator.GetStore()
+	if err != nil {
+		return nil, err
+	}
+
+	targetStore, err := d.targetCurator.GetStore()
+	if err != nil {
+		return nil, err
+	}
+
+	return baseStore.DiffStore(targetStore)
+}
+
+func (d *Differ) DeleteDatabases() error {
+	if err := d.baseCurator.Delete(); err != nil {
+		return fmt.Errorf("unable to delete vulnerability database: %+v", err)
+	}
+	if err := d.targetCurator.Delete(); err != nil {
+		return fmt.Errorf("unable to delete vulnerability database: %+v", err)
+	}
+	return nil
+}
+
+func (d *Differ) Present(outputFormat string, diff *[]v3.Diff, output io.Writer) error {
+	if diff == nil {
+		return nil
+	}
+
+	switch outputFormat {
+	case "table":
+		rows := [][]string{}
+		for _, d := range *diff {
+			rows = append(rows, []string{d.ID, d.Namespace, d.Reason})
+		}
+
+		table := tablewriter.NewWriter(os.Stdout)
+		columns := []string{"ID", "Namespace", "Reason"}
+
+		table.SetHeader(columns)
+		table.SetAutoWrapText(false)
+		table.SetHeaderAlignment(tablewriter.ALIGN_LEFT)
+		table.SetAlignment(tablewriter.ALIGN_LEFT)
+
+		table.SetHeaderLine(false)
+		table.SetBorder(false)
+		table.SetAutoFormatHeaders(true)
+		table.SetCenterSeparator("")
+		table.SetColumnSeparator("")
+		table.SetRowSeparator("")
+		table.SetTablePadding("  ")
+		table.SetNoWhiteSpace(true)
+
+		table.AppendBulk(rows)
+		table.Render()
+	case "json":
+		enc := json.NewEncoder(os.Stdout)
+		enc.SetEscapeHTML(false)
+		enc.SetIndent("", " ")
+		if err := enc.Encode(*diff); err != nil {
+			return fmt.Errorf("failed to encode diff information: %+v", err)
+		}
+	default:
+		return fmt.Errorf("unsupported output format: %s", outputFormat)
+	}
+	return nil
+}
