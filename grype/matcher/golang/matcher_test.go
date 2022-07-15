6570d067b420a827e53f622ef73db6b86f6bd51b
Christopher Phillips <christopher.phillips@anchore.com>
2022-07-15 12:07:36 -0400

    update testing for new main module exception
    Signed-off-by: Christopher Phillips <christopher.phillips@anchore.com>

--- grype/matcher/golang/matcher_test.go
+++ grype/matcher/golang/matcher_test.go
@@ -1,1 +1,70 @@
 package golang
+
+import (
+	"testing"
+
+	"github.com/anchore/grype/grype/distro"
+	"github.com/anchore/grype/grype/pkg"
+	"github.com/anchore/grype/grype/version"
+	"github.com/anchore/grype/grype/vulnerability"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+
+	syftPkg "github.com/anchore/syft/syft/pkg"
+)
+
+func TestMatcherGolang_DropMainPackage(t *testing.T) {
+	p := pkg.Package{
+		ID:           pkg.ID(uuid.NewString()),
+		Name:         "istio.io/istio",
+		Version:      "v0.0.0-20220606222826-f59ce19ec6b6",
+		Type:         syftPkg.GoModulePkg,
+		MetadataType: pkg.GolangBinMetadataType,
+		Metadata: pkg.GolangBinMetadata{
+			MainModule: "istio.io/istio",
+		},
+	}
+
+	matcher := Matcher{}
+	store := newMockProvider()
+
+	actual, _ := matcher.Match(store, nil, p)
+	assert.Len(t, actual, 0, "unexpected match count; should not match main module")
+}
+
+func newMockProvider() *mockProvider {
+	mp := mockProvider{
+		data: make(map[syftPkg.Language]map[string][]vulnerability.Vulnerability),
+	}
+
+	mp.populateData()
+
+	return &mp
+}
+
+type mockProvider struct {
+	data map[syftPkg.Language]map[string][]vulnerability.Vulnerability
+}
+
+func (mp *mockProvider) populateData() {
+	mp.data[syftPkg.Go] = map[string][]vulnerability.Vulnerability{
+		"istio.io/istio": {
+			{
+				Constraint: version.MustGetConstraint("<5.0.7", version.UnknownFormat),
+				ID:         "CVE-2013-fake-BAD",
+			},
+		},
+	}
+}
+
+func (mp *mockProvider) GetByCPE(p syftPkg.CPE) ([]vulnerability.Vulnerability, error) {
+	return []vulnerability.Vulnerability{}, nil
+}
+
+func (mp *mockProvider) GetByDistro(d *distro.Distro, p pkg.Package) ([]vulnerability.Vulnerability, error) {
+	return []vulnerability.Vulnerability{}, nil
+}
+
+func (mp *mockProvider) GetByLanguage(l syftPkg.Language, p pkg.Package) ([]vulnerability.Vulnerability, error) {
+	return mp.data[l][p.Name], nil
+}
