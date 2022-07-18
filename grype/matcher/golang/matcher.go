92a2d121f0b0096bf1479568d820badb1ef0f14f
Christopher Phillips <christopher.phillips@anchore.com>
2022-07-18 08:48:28 -0400

    static fix
    Signed-off-by: Christopher Phillips <christopher.phillips@anchore.com>

--- grype/matcher/golang/matcher.go
+++ grype/matcher/golang/matcher.go
@@ -1,13 +1,14 @@
 package golang
 
 import (
+	"strings"
+
 	"github.com/anchore/grype/grype/distro"
 	"github.com/anchore/grype/grype/match"
 	"github.com/anchore/grype/grype/pkg"
 	"github.com/anchore/grype/grype/search"
 	"github.com/anchore/grype/grype/vulnerability"
 	syftPkg "github.com/anchore/syft/syft/pkg"
-	"strings"
 )
 
 type Matcher struct {
