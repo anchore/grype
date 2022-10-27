ee1c0070d40acf16a6d4495654c158df0b4ff01b
Christopher Phillips <christopher.phillips@anchore.com>
2022-10-27 14:05:46 -0400

    fix: compare distro and not string
    Signed-off-by: Christopher Phillips <christopher.phillips@anchore.com>

--- grype/db/v5/namespace/index.go
+++ grype/db/v5/namespace/index.go
@@ -120,7 +120,7 @@ func (i *Index) NamespacesForDistro(d *grypeDistro.Distro) []*distro.Namespace {
 
 	// Fall back to alpine:edge if no version segments found
 	// alpine:edge is labeled as alpine-x.x_alphaYYYYMMDD
-	if versionSegments == nil && d.Type.String() == "alpine" {
+	if versionSegments == nil && d.Type == grypeDistro.Alpine {
 		distroKey := fmt.Sprintf("%s:%s", strings.ToLower(d.Type.String()), "edge")
 		if v, ok := i.byDistroKey[distroKey]; ok {
 			return v
