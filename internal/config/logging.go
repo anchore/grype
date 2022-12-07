5dda2f7fabce73d92217b5d2c8c24432c061c73e
Christopher Phillips <christopher.phillips@anchore.com>
2022-12-07 12:11:33 -0500

    chore: updated logging interface
    Signed-off-by: Christopher Phillips <christopher.phillips@anchore.com>

--- internal/config/logging.go
+++ internal/config/logging.go
@@ -1,15 +1,15 @@
 package config
 
 import (
-	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
+
+	"github.com/anchore/go-logger"
 )
 
 // logging contains all logging-related configuration options available to the user via the application config.
 type logging struct {
 	Structured   bool         `yaml:"structured" json:"structured" mapstructure:"structured"` // show all log entries as JSON formatted strings
-	LevelOpt     logrus.Level `yaml:"-" json:"-"`                                             // the native log level object used by the logger
-	Level        string       `yaml:"level" json:"level" mapstructure:"level"`                // the log level string hint
+	Level        logger.Level `yaml:"level" json:"level" mapstructure:"level"`                // the log level string hint
 	FileLocation string       `yaml:"file" json:"file" mapstructure:"file"`                   // the file path to write logs to
 }
 
