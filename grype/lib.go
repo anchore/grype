ab97ed5680e90eb27e40c9ae0ee9de213212f5cd
Christopher Phillips <christopher.phillips@anchore.com>
2022-12-08 13:55:07 -0500

    chore: fix merge conflicts
    Signed-off-by: Christopher Phillips <christopher.phillips@anchore.com>

--- grype/lib.go
+++ grype/lib.go
@@ -4,11 +4,6 @@ import (
 	"github.com/wagoodman/go-partybus"
 
 	"github.com/anchore/go-logger"
-	"github.com/anchore/grype/grype/db"
-	"github.com/anchore/grype/grype/match"
-	"github.com/anchore/grype/grype/matcher"
-	"github.com/anchore/grype/grype/pkg"
-	"github.com/anchore/grype/grype/store"
 	"github.com/anchore/grype/internal/bus"
 	"github.com/anchore/grype/internal/log"
 )
