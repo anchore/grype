7b367aa3ffc4a816bda9b350db7d6b9f0b91f2a6
Christopher Phillips <christopher.phillips@anchore.com>
2022-12-14 11:04:55 -0500

    update golden image and unit test coverage
    Signed-off-by: Christopher Phillips <christopher.phillips@anchore.com>

--- grype/presenter/cyclonedx/document.go
+++ grype/presenter/cyclonedx/document.go
@@ -1,21 +1,12 @@
 package cyclonedx
 
-import (
-	"github.com/CycloneDX/cyclonedx-go"
-
-	"github.com/anchore/grype/grype/match"
-	"github.com/anchore/grype/grype/pkg"
-	"github.com/anchore/grype/grype/vulnerability"
-	"github.com/anchore/syft/syft/source"
-)
-
-// NewDocument returns a CycloneDX Document object populated with the SBOM and vulnerability findings.
-func NewDocument(
-	packages []pkg.Package,
-	matches match.Matches,
-	srcMetadata *source.Metadata,
-	provider vulnerability.MetadataProvider,
-) *cyclonedx.BOM {
-	cdxBom := cyclonedx.NewBOM()
-	return cdxBom
-}
+//// NewDocument returns a CycloneDX Document object populated with the SBOM and vulnerability findings.
+//func NewDocument(
+//	packages []pkg.Package,
+//	matches match.Matches,
+//	srcMetadata *source.Metadata,
+//	provider vulnerability.MetadataProvider,
+//) *cyclonedx.BOM {
+//	cdxBom := cyclonedx.NewBOM()
+//	return cdxBom
+//}
--- grype/presenter/cyclonedx/presenter_test.go
+++ grype/presenter/cyclonedx/presenter_test.go
@@ -3,12 +3,11 @@ package cyclonedx
 import (
 	"bytes"
 	"flag"
+	"github.com/anchore/go-testutils"
+	"github.com/google/go-cmp/cmp"
 	"testing"
 
 	"github.com/CycloneDX/cyclonedx-go"
-	"github.com/sergi/go-diff/diffmatchpatch"
-
-	"github.com/anchore/go-testutils"
 	"github.com/anchore/grype/grype/presenter/models"
 	"github.com/anchore/syft/syft/sbom"
 	"github.com/anchore/syft/syft/source"
@@ -16,80 +15,67 @@ import (
 
 var update = flag.Bool("update", false, "update the *.golden files for cyclonedx presenters")
 
-func TestCycloneDxPresenterImage(t *testing.T) {
-	var buffer bytes.Buffer
-
-	matches, packages, context, metadataProvider, _, _ := models.GenerateAnalysis(t, source.ImageScheme)
-
-	pb := models.PresenterBundle{
-		Matches:          &matches,
-		Packages:         packages,
-		Context:          context,
-		MetadataProvider: metadataProvider,
-		SBOM:             &sbom.SBOM{},
+// Note: this is a unit test for only encoding the vulnerability matches to a CycloneDX document.
+// The full integration test will cover regressions for the entire document. When actual SBOM source are used
+func TestCycloneDxPresenter(t *testing.T) {
+	tests := []struct {
+		name   string
+		scheme source.Scheme
+		format cyclonedx.BOMFileFormat
+	}{
+		{
+			name:   "image scheme: cyclonedx-json",
+			scheme: source.ImageScheme,
+			format: cyclonedx.BOMFileFormatJSON,
+		},
+		{
+			name:   "image scheme: cyclonedx-xml",
+			scheme: source.ImageScheme,
+			format: cyclonedx.BOMFileFormatXML,
+		},
+		{
+			name:   "directory scheme: cyclonedx-json",
+			scheme: source.DirectoryScheme,
+			format: cyclonedx.BOMFileFormatJSON,
+		},
+		{
+			name:   "directory scheme: cyclonedx-xml",
+			scheme: source.DirectoryScheme,
+			format: cyclonedx.BOMFileFormatXML,
+		},
 	}
 
-	pres := NewPresenter(pb, cyclonedx.BOMFileFormatJSON)
-	// run presenter
-	err := pres.Present(&buffer)
-	if err != nil {
-		t.Fatal(err)
+	for _, test := range tests {
+		var buffer bytes.Buffer
+		matches, packages, context, metadataProvider, _, _ := models.GenerateAnalysis(t, test.scheme)
+
+		pb := models.PresenterBundle{
+			Matches:          &matches,
+			Packages:         packages,
+			Context:          context,
+			MetadataProvider: metadataProvider,
+			SBOM:             &sbom.SBOM{},
+		}
+
+		pres := NewPresenter(pb, test.format)
+		// run presenter
+		err := pres.Present(&buffer)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		got := buffer.Bytes()
+		testutils.UpdateGoldenFileContents(t, got)
+
+		want := testutils.GetGoldenFileContents(t)
+		//// remove dynamic values, which are tested independently
+		want = models.Redact(want)
+		got = models.Redact(got)
+
+		if !bytes.Equal(want, got) {
+			if diff := cmp.Diff(want, got); diff != "" {
+				t.Errorf("pres.Present mismatch: %s: (-want +got):\n%s", test.name, diff)
+			}
+		}
 	}
-
-	actual := buffer.Bytes()
-	if *update {
-		testutils.UpdateGoldenFileContents(t, actual)
-	}
-
-	var expected = testutils.GetGoldenFileContents(t)
-
-	// remove dynamic values, which are tested independently
-	actual = models.Redact(actual)
-	expected = models.Redact(expected)
-
-	if !bytes.Equal(expected, actual) {
-		dmp := diffmatchpatch.New()
-		diffs := dmp.DiffMain(string(expected), string(actual), true)
-		t.Errorf("mismatched output:\n%s", dmp.DiffPrettyText(diffs))
-	}
-
-}
-
-func TestCycloneDxPresenterDir(t *testing.T) {
-	var buffer bytes.Buffer
-	matches, packages, ctx, metadataProvider, _, _ := models.GenerateAnalysis(t, source.DirectoryScheme)
-
-	pb := models.PresenterBundle{
-		Matches:          &matches,
-		Packages:         packages,
-		Context:          ctx,
-		MetadataProvider: metadataProvider,
-		SBOM:             &sbom.SBOM{},
-	}
-
-	pres := NewPresenter(pb, cyclonedx.BOMFileFormatJSON)
-
-	// run presenter
-	err := pres.Present(&buffer)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	actual := buffer.Bytes()
-	if *update {
-		testutils.UpdateGoldenFileContents(t, actual)
-	}
-
-	var expected = testutils.GetGoldenFileContents(t)
-
-	// remove dynamic values, which are tested independently
-	actual = models.Redact(actual)
-	expected = models.Redact(expected)
-
-	if !bytes.Equal(expected, actual) {
-		dmp := diffmatchpatch.New()
-		diffs := dmp.DiffMain(string(expected), string(actual), true)
-		t.Errorf("mismatched output:\n%s", dmp.DiffPrettyText(diffs))
-	}
-
 }
