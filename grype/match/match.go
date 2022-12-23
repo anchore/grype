d4285d67a3bd55f0f1a2735f0ca9679bb081d42c
Keith Zantow <kzantow@gmail.com>
2022-12-23 12:44:50 -0500

    fix: orient by cve merging
    Signed-off-by: Keith Zantow <kzantow@gmail.com>

--- grype/match/match.go
+++ grype/match/match.go
@@ -2,12 +2,14 @@ package match
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 
 	"github.com/scylladb/go-set/strset"
 
 	"github.com/anchore/grype/grype/pkg"
 	"github.com/anchore/grype/grype/vulnerability"
+	syftPkg "github.com/anchore/syft/syft/pkg"
 )
 
 var ErrCannotMerge = fmt.Errorf("unable to merge vulnerability matches")
@@ -42,17 +44,68 @@ func (m *Match) Merge(other Match) error {
 		return ErrCannotMerge
 	}
 
+	// there are cases related vulnerabilities are synthetic, for example when
+	// orienting results by CVE. we need to keep track of these
+	related := strset.New()
+	for _, r := range m.Vulnerability.RelatedVulnerabilities {
+		related.Add(referenceID(r))
+	}
+	for _, r := range other.Vulnerability.RelatedVulnerabilities {
+		if related.Has(referenceID(r)) {
+			continue
+		}
+		m.Vulnerability.RelatedVulnerabilities = append(m.Vulnerability.RelatedVulnerabilities, r)
+	}
+
+	// for stable output
+	sort.Slice(m.Vulnerability.RelatedVulnerabilities, func(i, j int) bool {
+		a := m.Vulnerability.RelatedVulnerabilities[i]
+		b := m.Vulnerability.RelatedVulnerabilities[j]
+		return strings.Compare(referenceID(a), referenceID(b)) < 0
+	})
+
+	// similarly we need to retain unique CPEs for consistent output
+	cpes := strset.New()
+	for _, c := range m.Vulnerability.CPEs {
+		cpes.Add(syftPkg.CPEString(c))
+	}
+	for _, c := range other.Vulnerability.CPEs {
+		if cpes.Has(syftPkg.CPEString(c)) {
+			continue
+		}
+		m.Vulnerability.CPEs = append(m.Vulnerability.CPEs, c)
+	}
+
+	// for stable output
+	sort.Slice(m.Vulnerability.CPEs, func(i, j int) bool {
+		a := m.Vulnerability.CPEs[i]
+		b := m.Vulnerability.CPEs[j]
+		return strings.Compare(syftPkg.CPEString(a), syftPkg.CPEString(b)) < 0
+	})
+
+	// also keep details from the other match that are unique
 	detailIDs := strset.New()
 	for _, d := range m.Details {
 		detailIDs.Add(d.ID())
 	}
-
-	// keep details from the other match that are unique
 	for _, d := range other.Details {
 		if detailIDs.Has(d.ID()) {
 			continue
 		}
 		m.Details = append(m.Details, d)
 	}
+
+	// for stable output
+	sort.Slice(m.Details, func(i, j int) bool {
+		a := m.Details[i]
+		b := m.Details[j]
+		return strings.Compare(a.ID(), b.ID()) < 0
+	})
+
 	return nil
+}
+
+// referenceID returns an "ID" string for a vulnerability.Reference
+func referenceID(r vulnerability.Reference) string {
+	return fmt.Sprintf("%s:%s", r.Namespace, r.ID)
 }
