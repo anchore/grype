bdcefd255480b2ce9566e5e122204c25d518cdd8
Keith Zantow <kzantow@gmail.com>
2023-03-01 16:47:01 -0500

    chore: update progress monitor handling (#1149)

--- grype/matcher/matchers.go
+++ grype/matcher/matchers.go
@@ -117,7 +117,7 @@ func FindMatches(store interface {
 		defaultMatcher = stock.NewStockMatcher(stock.MatcherConfig{UseCPEs: true})
 	}
 	for _, p := range packages {
-		packagesProcessed.N++
+		packagesProcessed.Increment()
 		log.Debugf("searching for vulnerability matches for pkg=%s", p)
 
 		matchAgainst, ok := matcherIndex[p.Type]
@@ -131,7 +131,7 @@ func FindMatches(store interface {
 			} else {
 				logMatches(p, matches)
 				res.Add(matches...)
-				vulnerabilitiesDiscovered.N += int64(len(matches))
+				vulnerabilitiesDiscovered.Add(int64(len(matches)))
 			}
 		}
 	}
