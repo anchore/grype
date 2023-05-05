57b0d2d3f25cdf4808bd4ccee3511b5e35423c42
James Neate <jamesmneate@gmail.com>
2023-05-05 22:17:51 +0100

    feat: add non-hermetic sprig functions (#1243)
    Signed-off-by: James Neate <jamesmneate@gmail.com>

--- grype/presenter/template/presenter_test.go
+++ grype/presenter/template/presenter_test.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"os"
 	"path"
+	"regexp"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -15,6 +16,7 @@ import (
 )
 
 var update = flag.Bool("update", false, "update the *.golden files for template presenters")
+var timestampRegexp = regexp.MustCompile(`Timestamp:\s*[0-9\-]+`)
 
 func TestPresenter_Present(t *testing.T) {
 	matches, packages, context, metadataProvider, appConfig, dbStatus := models.GenerateAnalysis(t, source.ImageScheme)
@@ -42,10 +44,16 @@ func TestPresenter_Present(t *testing.T) {
 	}
 
 	actual := buffer.Bytes()
+	actual = redact(actual)
+
 	if *update {
 		testutils.UpdateGoldenFileContents(t, actual)
 	}
 	expected := testutils.GetGoldenFileContents(t)
 
 	assert.Equal(t, string(expected), string(actual))
+}
+
+func redact(content []byte) []byte {
+	return timestampRegexp.ReplaceAll(content, []byte(`Timestamp:`))
 }
