2ebb7b2514e5ff322a8dbe5de4c15ed96c02a4f1
Will Murphy <will.murphy@anchore.com>
2023-05-08 12:26:07 -0400

    Allow generic sprig functions
    Previously, functions that could return different results at different
    times, such as `now`, were not allowed in sprig templates. Allow them.
    
    Signed-off-by: Will Murphy <will.murphy@anchore.com>

--- grype/presenter/template/presenter_test.go
+++ grype/presenter/template/presenter_test.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"os"
 	"path"
+	"regexp"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -48,4 +49,34 @@ func TestPresenter_Present(t *testing.T) {
 	expected := testutils.GetGoldenFileContents(t)
 
 	assert.Equal(t, string(expected), string(actual))
+}
+
+func TestPresenter_SprigDate(t *testing.T) {
+	matches, packages, context, metadataProvider, appConfig, dbStatus := models.GenerateAnalysis(t, source.ImageScheme)
+	workingDirectory, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+	templateFilePath := path.Join(workingDirectory, "./test-fixtures/test.template.sprig")
+
+	pb := models.PresenterConfig{
+		Matches:          matches,
+		Packages:         packages,
+		Context:          context,
+		MetadataProvider: metadataProvider,
+		AppConfig:        appConfig,
+		DBStatus:         dbStatus,
+	}
+
+	templatePresenter := NewPresenter(pb, templateFilePath)
+
+	var buffer bytes.Buffer
+	if err := templatePresenter.Present(&buffer); err != nil {
+		t.Fatal(err)
+	}
+
+	actual := string(buffer.Bytes())
+	dateRexexp := regexp.MustCompile("\\d{4}-\\d{2}-\\d{2}")
+
+	assert.True(t, dateRexexp.MatchString(actual))
 }
