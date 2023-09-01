f559463dfba5cbe48fc88e1ffce0b3d50dd1065d
Will Murphy <will.murphy@anchore.com>
2023-09-01 08:33:43 -0400

    add snapshot test and fixtures
    Signed-off-by: Will Murphy <will.murphy@anchore.com>

---
+++ grype/presenter/explain/explain_snapshot_test.go
@@ -0,0 +1,36 @@
+package explain_test
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"os"
+	"testing"
+
+	"github.com/anchore/grype/grype/presenter/explain"
+	"github.com/anchore/grype/grype/presenter/models"
+	"github.com/gkampitakis/go-snaps/snaps"
+	"github.com/stretchr/testify/require"
+)
+
+func TestExplainSnapshot(t *testing.T) {
+	// load sample json
+	// TODO: make and commit minimal sample JSON
+	r, err := os.Open("./test-fixtures/keycloak-test.json")
+	require.NoError(t, err)
+
+	// parse to models.Document
+	doc := models.Document{}
+	decoder := json.NewDecoder(r)
+	err = decoder.Decode(&doc)
+	require.NoError(t, err)
+	// create explain.VulnerabilityExplainer
+	var b bytes.Buffer
+	w := bufio.NewWriter(&b)
+	explainer := explain.NewVulnerabilityExplainer(w, &doc)
+	// call ExplainByID
+	err = explainer.ExplainByID([]string{"CVE-2020-12413"})
+	require.NoError(t, err)
+	// assert output
+	snaps.MatchSnapshot(t, b.Bytes())
+}
