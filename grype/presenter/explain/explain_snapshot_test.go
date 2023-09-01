d62b645419eea9823e733e9aa5e83189e13fd5b1
Will Murphy <will.murphy@anchore.com>
2023-09-01 14:29:47 -0400

    add additional test case
    Signed-off-by: Will Murphy <will.murphy@anchore.com>

--- grype/presenter/explain/explain_snapshot_test.go
+++ grype/presenter/explain/explain_snapshot_test.go
@@ -15,21 +15,41 @@ import (
 
 func TestExplainSnapshot(t *testing.T) {
 	// load sample json
-	// TODO: make and commit minimal sample JSON
-	r, err := os.Open("./test-fixtures/keycloak-test.json")
-	require.NoError(t, err)
+	testCases := []struct {
+		name             string
+		fixture          string
+		vulnerabilityIDs []string
+	}{
+		{
+			name:             "keycloak-CVE-2020-12413",
+			fixture:          "./test-fixtures/keycloak-test.json",
+			vulnerabilityIDs: []string{"CVE-2020-12413"},
+		},
+		{
+			name:             "chainguard-ruby-CVE-2023-28755",
+			fixture:          "test-fixtures/chainguard-ruby-test.json",
+			vulnerabilityIDs: []string{"CVE-2023-28755"},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			r, err := os.Open(tc.fixture)
+			require.NoError(t, err)
 
-	// parse to models.Document
-	doc := models.Document{}
-	decoder := json.NewDecoder(r)
-	err = decoder.Decode(&doc)
-	require.NoError(t, err)
-	// create explain.VulnerabilityExplainer
-	w := bytes.NewBufferString("")
-	explainer := explain.NewVulnerabilityExplainer(w, &doc)
-	// call ExplainByID
-	err = explainer.ExplainByID([]string{"CVE-2020-12413"})
-	require.NoError(t, err)
-	// assert output
-	snaps.MatchSnapshot(t, w.String())
+			// parse to models.Document
+			doc := models.Document{}
+			decoder := json.NewDecoder(r)
+			err = decoder.Decode(&doc)
+			require.NoError(t, err)
+			// create explain.VulnerabilityExplainer
+			w := bytes.NewBufferString("")
+			explainer := explain.NewVulnerabilityExplainer(w, &doc)
+			// call ExplainByID
+			err = explainer.ExplainByID(tc.vulnerabilityIDs)
+			require.NoError(t, err)
+			// assert output
+			snaps.MatchSnapshot(t, w.String())
+		})
+	}
+
 }
