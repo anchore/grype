f06bde0081f6d97fafd687390daf6267d8c364c6
Weston Steimel <weston.steimel@anchore.com>
2024-01-24 15:07:11 +0000

    hack: slightly less horrible hack
    Signed-off-by: Weston Steimel <weston.steimel@anchore.com>

--- grype/search/only_qualified_packages.go
+++ grype/search/only_qualified_packages.go
@@ -5,11 +5,7 @@ import (
 
 	"github.com/anchore/grype/grype/distro"
 	"github.com/anchore/grype/grype/pkg"
-
-	"github.com/anchore/grype/grype/pkg/qualifier/rpmmodularity"
 	"github.com/anchore/grype/grype/vulnerability"
-	_ "github.com/anchore/grype/internal/log"
-	syftPkg "github.com/anchore/syft/syft/pkg"
 )
 
 func onlyQualifiedPackages(d *distro.Distro, p pkg.Package, allVulns []vulnerability.Vulnerability) ([]vulnerability.Vulnerability, error) {
@@ -17,12 +13,6 @@ func onlyQualifiedPackages(d *distro.Distro, p pkg.Package, allVulns []vulnerabi
 
 	for _, vuln := range allVulns {
 		isVulnerable := true
-
-		if p.Type == syftPkg.RpmPkg {
-			if len(vuln.PackageQualifiers) == 0 {
-				vuln.PackageQualifiers = append(vuln.PackageQualifiers, rpmmodularity.New(""))
-			}
-		}
 
 		for _, q := range vuln.PackageQualifiers {
 			v, err := q.Satisfied(d, p)
