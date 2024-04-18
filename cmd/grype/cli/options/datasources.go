4eb934b5ad2312305322176c1f3ad33463f70853
Pouyan Khodabakhsh <pouyan021@gmail.com>
2024-04-18 13:00:10 +0400

    adding abort-after property to external-sources
    Signed-off-by: Pouyan Khodabakhsh <pouyan021@gmail.com>

--- cmd/grype/cli/options/datasources.go
+++ cmd/grype/cli/options/datasources.go
@@ -9,11 +9,13 @@ import (
 
 const (
 	defaultMavenBaseURL = "https://search.maven.org/solrsearch/select"
+	defaultAbortAfter   = "10m"
 )
 
 type externalSources struct {
-	Enable bool  `yaml:"enable" json:"enable" mapstructure:"enable"`
-	Maven  maven `yaml:"maven" json:"maven" mapstructure:"maven"`
+	Enable     bool   `yaml:"enable" json:"enable" mapstructure:"enable"`
+	AbortAfter string `yaml:"abort-after" json:"abortAfter" mapstructure:"abort-after"`
+	Maven      maven  `yaml:"maven" json:"maven" mapstructure:"maven"`
 }
 
 var _ interface {
@@ -21,13 +23,15 @@ var _ interface {
 } = (*externalSources)(nil)
 
 type maven struct {
-	SearchUpstreamBySha1 bool          `yaml:"search-upstream" json:"searchUpstreamBySha1" mapstructure:"search-maven-upstream"`
-	BaseURL              string        `yaml:"base-url" json:"baseUrl" mapstructure:"base-url"`
+	SearchUpstreamBySha1 bool    `yaml:"search-upstream" json:"searchUpstreamBySha1" mapstructure:"search-maven-upstream"`
+	BaseURL              string  `yaml:"base-url" json:"baseUrl" mapstructure:"base-url"`
+	AbortAfter           *string `yaml:"abort-after" json:"abortAfter" mapstructure:"abort-after"`
 	RateLimit            time.Duration `yaml:"rate-limit" json:"rateLimit" mapstructure:"rate-limit"`
 }
 
 func defaultExternalSources() externalSources {
 	return externalSources{
+		AbortAfter: defaultAbortAfter,
 		Maven: maven{
 			SearchUpstreamBySha1: true,
 			BaseURL:              defaultMavenBaseURL,
@@ -42,9 +46,16 @@ func (cfg externalSources) ToJavaMatcherConfig() java.ExternalSearchConfig {
 	if !cfg.Enable {
 		smu = cfg.Enable
 	}
+
+	abortAfter := cfg.AbortAfter
+	if cfg.Maven.AbortAfter != nil {
+		abortAfter = *cfg.Maven.AbortAfter
+	}
+
 	return java.ExternalSearchConfig{
 		SearchMavenUpstream: smu,
 		MavenBaseURL:        cfg.Maven.BaseURL,
+		AbortAfter:          abortAfter,
 		MavenRateLimit:      cfg.Maven.RateLimit,
 	}
 }
