24d5d4ffb23d48181cd6aa0e8522f727ad357ce6
Alex Goodman <wagoodman@users.noreply.github.com>
2024-05-09 16:25:00 -0400

    Upgrade tool management (#1842)
    * upgrade tool management
    
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>
    
    * update version file on release
    
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>

--- .github/scripts/update-version-file.sh
+++ .github/scripts/update-version-file.sh
@@ -2,8 +2,8 @@
 set -ue
 
 BIN="grype"
-DISTDIR=$1
-VERSION=$2
+VERSION_FILE="VERSION"
+VERSION=$1
 
 # the source of truth as to whether we want to notify users of an update is if the release just created is NOT
 # flagged as a pre-release on github
@@ -12,10 +12,9 @@ if [[ "$(curl -SsL https://api.github.com/repos/anchore/${BIN}/releases/tags/${V
    exit 0
 fi
 
-echo "creating and publishing version file"
+echo "creating and publishing version file (${VERSION})"
 
 # create a version file for version-update checks
-VERSION_FILE="${DISTDIR}/VERSION"
 echo "${VERSION}" | tee "${VERSION_FILE}"
 
 # upload the version file that supports the application version update check
