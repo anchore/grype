ac55fb356e8a909b41fe3f849928cd90699d4568
Christopher Phillips <32073428+spiffcs@users.noreply.github.com>
2024-06-24 13:43:37 -0400

    chore: change relative path for Make context
    Signed-off-by: Christopher Phillips <32073428+spiffcs@users.noreply.github.com>

--- .github/scripts/update-quality-gate-db.sh
+++ .github/scripts/update-quality-gate-db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run your command and capture the output
-output=$(go run ../../cmd/grype/main.go db list)
+output=$(go run ./cmd/grype/main.go db list)
 
 # Extract the first instance of URL using grep, cut, and sed to trim leading whitespace
 url=$(echo "$output" | grep -m 1 -o 'URL: .*' | cut -d' ' -f2- | sed 's/^[[:space:]]*//')
@@ -10,4 +10,4 @@ url=$(echo "$output" | grep -m 1 -o 'URL: .*' | cut -d' ' -f2- | sed 's/^[[:spac
 escaped_url=$(printf '%s\n' "$url" | sed -e 's/[\/&]/\\&/g')
 
 # Replace TEST_DB_URL in specific Makefile using sed
-sed -i '' -e "s|^TEST_DB_URL = .*|TEST_DB_URL = $escaped_url|" ../../test/quality/Makefile
+sed -i '' -e "s|^TEST_DB_URL = .*|TEST_DB_URL = $escaped_url|" ./test/quality/Makefile
