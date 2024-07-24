fe3adfbc993c15f25e60f6a3e505e6ccaad06ed8
Alex Goodman <wagoodman@users.noreply.github.com>
2024-07-24 16:03:30 -0400

    disable ui before run function on db status
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>

--- cmd/grype/cli/commands/db_status.go
+++ cmd/grype/cli/commands/db_status.go
@@ -14,10 +14,10 @@ func DBStatus(app clio.Application) *cobra.Command {
 	opts := dbOptionsDefault(app.ID())
 
 	return app.SetupCommand(&cobra.Command{
-		Use:      "status",
-		Short:    "display database status",
-		Args:     cobra.ExactArgs(0),
-		PostRunE: disableUI(app),
+		Use:     "status",
+		Short:   "display database status",
+		Args:    cobra.ExactArgs(0),
+		PreRunE: disableUI(app),
 		RunE: func(_ *cobra.Command, _ []string) error {
 			return runDBStatus(opts.DB)
 		},
