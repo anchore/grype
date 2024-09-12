14b1a8337eb1d7d6f6829e10d60e3b589aca0732
Keith Zantow <kzantow@gmail.com>
2024-09-12 11:50:20 -0400

    feat: --enrich flag to enable data enrichment
    Signed-off-by: Keith Zantow <kzantow@gmail.com>

--- cmd/grype/cli/options/datasources.go
+++ cmd/grype/cli/options/datasources.go
@@ -2,7 +2,6 @@ package options
 
 import (
 	"github.com/anchore/clio"
-	"github.com/anchore/grype/grype/matcher/java"
 )
 
 const (
@@ -10,7 +9,7 @@ const (
 )
 
 type externalSources struct {
-	Enable bool  `yaml:"enable" json:"enable" mapstructure:"enable"`
+	Enable *bool `yaml:"enable" json:"enable" mapstructure:"enable"`
 	Maven  maven `yaml:"maven" json:"maven" mapstructure:"maven"`
 }
 
@@ -19,28 +18,16 @@ var _ interface {
 } = (*externalSources)(nil)
 
 type maven struct {
-	SearchUpstreamBySha1 bool   `yaml:"search-upstream" json:"searchUpstreamBySha1" mapstructure:"search-maven-upstream"`
+	SearchUpstreamBySha1 *bool  `yaml:"search-upstream" json:"searchUpstreamBySha1" mapstructure:"search-maven-upstream"`
 	BaseURL              string `yaml:"base-url" json:"baseUrl" mapstructure:"base-url"`
 }
 
 func defaultExternalSources() externalSources {
 	return externalSources{
 		Maven: maven{
-			SearchUpstreamBySha1: true,
+			SearchUpstreamBySha1: nil,
 			BaseURL:              defaultMavenBaseURL,
 		},
-	}
-}
-
-func (cfg externalSources) ToJavaMatcherConfig() java.ExternalSearchConfig {
-	// always respect if global config is disabled
-	smu := cfg.Maven.SearchUpstreamBySha1
-	if !cfg.Enable {
-		smu = cfg.Enable
-	}
-	return java.ExternalSearchConfig{
-		SearchMavenUpstream: smu,
-		MavenBaseURL:        cfg.Maven.BaseURL,
 	}
 }
 
