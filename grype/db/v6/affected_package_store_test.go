df5442171da373ac5be2bdc5d34be32f260b08aa
Alex Goodman <wagoodman@users.noreply.github.com>
2024-11-21 10:27:20 -0500

    allow distro search to be entirely data driven (#2265)
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>

--- grype/db/v6/affected_package_store_test.go
+++ grype/db/v6/affected_package_store_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -57,6 +58,7 @@ func TestAffectedPackageStore_GetAffectedPackagesByName(t *testing.T) {
 		packageName string
 		options     *GetAffectedPackageOptions
 		expected    []AffectedPackageHandle
+		wantErr     require.ErrorAssertionFunc
 	}{
 		{
 			name:        "specific distro",
@@ -71,15 +73,28 @@ func TestAffectedPackageStore_GetAffectedPackagesByName(t *testing.T) {
 			expected: []AffectedPackageHandle{*pkg2d1},
 		},
 		{
-			name:        "distro major version",
+			name:        "distro major version only (allow multiple)",
 			packageName: pkg2d1.Package.Name,
 			options: &GetAffectedPackageOptions{
 				Distro: &DistroSpecifier{
-					Name:         "ubuntu",
-					MajorVersion: "20",
+					Name:          "ubuntu",
+					MajorVersion:  "20",
+					AllowMultiple: true,
 				},
 			},
 			expected: []AffectedPackageHandle{*pkg2d1, *pkg2d2},
+		},
+		{
+			name:        "distro major version only (default)",
+			packageName: pkg2d1.Package.Name,
+			options: &GetAffectedPackageOptions{
+				Distro: &DistroSpecifier{
+					Name:          "ubuntu",
+					MajorVersion:  "20",
+					AllowMultiple: false,
+				},
+			},
+			wantErr: expectErrIs(t, ErrMultipleOSMatches),
 		},
 		{
 			name:        "distro codename",
@@ -203,9 +218,12 @@ func TestAffectedPackageStore_GetAffectedPackagesByName(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-
+			if tt.wantErr == nil {
+				tt.wantErr = require.NoError
+			}
 			for _, pc := range preloadCases {
 				t.Run(pc.name, func(t *testing.T) {
+
 					opts := tt.options
 					opts.PreloadOS = pc.PreloadOS
 					opts.PreloadPackage = pc.PreloadPackage
@@ -215,11 +233,225 @@ func TestAffectedPackageStore_GetAffectedPackagesByName(t *testing.T) {
 						expected = pc.prepExpectations(t, expected)
 					}
 					result, err := s.GetAffectedPackagesByName(tt.packageName, opts)
-					require.NoError(t, err)
+					tt.wantErr(t, err)
+					if err != nil {
+						return
+					}
 					if d := cmp.Diff(expected, result); d != "" {
 						t.Errorf(fmt.Sprintf("unexpected result: %s", d))
 					}
 				})
+			}
+		})
+	}
+}
+
+func TestAffectedPackageStore_ResolveDistro(t *testing.T) {
+	db := setupTestStore(t).db
+	bs := newBlobStore(db)
+	s := newAffectedPackageStore(db, bs)
+
+	aliases := []OperatingSystemAlias{
+		{Name: "centos", ReplacementName: strRef("rhel")},
+		{Name: "rocky", ReplacementName: strRef("rhel")},
+		{Name: "alpine", VersionPattern: ".*_alpha.*", ReplacementLabelVersion: strRef("edge"), Rolling: true},
+		{Name: "wolfi", Rolling: true},
+		{Name: "arch", Rolling: true},
+		{Name: "debian", Codename: "trixie", Rolling: true}, // is currently sid, which is considered rolling
+	}
+	require.NoError(t, db.Create(&aliases).Error)
+
+	ubuntu2004 := &OperatingSystem{Name: "ubuntu", MajorVersion: "20", MinorVersion: "04", Codename: "focal"}
+	ubuntu2010 := &OperatingSystem{Name: "ubuntu", MajorVersion: "20", MinorVersion: "10", Codename: "groovy"}
+	rhel8 := &OperatingSystem{Name: "rhel", MajorVersion: "8"}
+	rhel81 := &OperatingSystem{Name: "rhel", MajorVersion: "8", MinorVersion: "1"}
+	debian10 := &OperatingSystem{Name: "debian", MajorVersion: "10"}
+	alpine318 := &OperatingSystem{Name: "alpine", MajorVersion: "3", MinorVersion: "18"}
+	alpineEdge := &OperatingSystem{Name: "alpine", LabelVersion: "edge"}
+	debianTrixie := &OperatingSystem{Name: "debian", Codename: "trixie"}
+	debian7 := &OperatingSystem{Name: "debian", MajorVersion: "7", Codename: "wheezy"}
+	wolfi := &OperatingSystem{Name: "wolfi", MajorVersion: "20230201"}
+	arch := &OperatingSystem{Name: "arch", MajorVersion: "20241110", MinorVersion: "0"}
+
+	operatingSystems := []*OperatingSystem{
+		ubuntu2004,
+		ubuntu2010,
+		rhel8,
+		rhel81,
+		debian10,
+		alpine318,
+		alpineEdge,
+		debianTrixie,
+		debian7,
+		wolfi,
+		arch,
+	}
+	require.NoError(t, db.Create(&operatingSystems).Error)
+
+	tests := []struct {
+		name      string
+		distro    DistroSpecifier
+		expected  []OperatingSystem
+		expectErr require.ErrorAssertionFunc
+	}{
+		{
+			name: "specific distro with major and minor version",
+			distro: DistroSpecifier{
+				Name:         "ubuntu",
+				MajorVersion: "20",
+				MinorVersion: "04",
+			},
+			expected: []OperatingSystem{*ubuntu2004},
+		},
+		{
+			name: "alias resolution with major version",
+			distro: DistroSpecifier{
+				Name:         "centos",
+				MajorVersion: "8",
+			},
+			expected: []OperatingSystem{*rhel8},
+		},
+		{
+			name: "alias resolution with major and minor version",
+			distro: DistroSpecifier{
+				Name:         "centos",
+				MajorVersion: "8",
+				MinorVersion: "1",
+			},
+			expected: []OperatingSystem{*rhel81},
+		},
+		{
+			name: "distro with major version only",
+			distro: DistroSpecifier{
+				Name:         "debian",
+				MajorVersion: "10",
+			},
+			expected: []OperatingSystem{*debian10},
+		},
+		{
+			name: "codename resolution",
+			distro: DistroSpecifier{
+				Name:     "ubuntu",
+				Codename: "focal",
+			},
+			expected: []OperatingSystem{*ubuntu2004},
+		},
+		{
+			name: "codename and version info",
+			distro: DistroSpecifier{
+				Name:         "ubuntu",
+				MajorVersion: "20",
+				MinorVersion: "04",
+				Codename:     "focal",
+			},
+			expected: []OperatingSystem{*ubuntu2004},
+		},
+		{
+			name: "conflicting codename and version info",
+			distro: DistroSpecifier{
+				Name:         "ubuntu",
+				MajorVersion: "20",
+				MinorVersion: "04",
+				Codename:     "fake",
+			},
+		},
+		{
+			name: "alpine edge version",
+			distro: DistroSpecifier{
+				Name:         "alpine",
+				MajorVersion: "3",
+				MinorVersion: "21",
+				LabelVersion: "3.21.0_alpha20240807",
+			},
+			expected: []OperatingSystem{*alpineEdge},
+		},
+		{
+			name: "arch rolling variant",
+			distro: DistroSpecifier{
+				Name: "arch",
+			},
+			expected: []OperatingSystem{*arch},
+		},
+		{
+			name: "wolfi rolling variant",
+			distro: DistroSpecifier{
+				Name:         "wolfi",
+				MajorVersion: "20221018",
+			},
+			expected: []OperatingSystem{*wolfi},
+		},
+		{
+			name: "debian by codename for rolling alias",
+			distro: DistroSpecifier{
+				Name:         "debian",
+				MajorVersion: "13",
+				Codename:     "trixie", // TODO: what about sid status indication from pretty-name or /etc/debian_version?
+			},
+			expected: []OperatingSystem{*debianTrixie},
+		},
+		{
+			name: "debian by codename",
+			distro: DistroSpecifier{
+				Name:     "debian",
+				Codename: "wheezy",
+			},
+			expected: []OperatingSystem{*debian7},
+		},
+		{
+			name: "debian by major version",
+			distro: DistroSpecifier{
+				Name:         "debian",
+				MajorVersion: "7",
+			},
+			expected: []OperatingSystem{*debian7},
+		},
+		{
+			name: "debian by major.minor version",
+			distro: DistroSpecifier{
+				Name:         "debian",
+				MajorVersion: "7",
+				MinorVersion: "2",
+			},
+			expected: []OperatingSystem{*debian7},
+		},
+		{
+			name: "alpine with major and minor version",
+			distro: DistroSpecifier{
+				Name:         "alpine",
+				MajorVersion: "3",
+				MinorVersion: "18",
+			},
+			expected: []OperatingSystem{*alpine318},
+		},
+		{
+			name: "missing distro name",
+			distro: DistroSpecifier{
+				MajorVersion: "8",
+			},
+			expectErr: expectErrIs(t, ErrMissingDistroIdentification),
+		},
+		{
+			name: "nonexistent distro",
+			distro: DistroSpecifier{
+				Name:         "madeup",
+				MajorVersion: "99",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.expectErr == nil {
+				tt.expectErr = require.NoError
+			}
+			result, err := s.resolveDistro(tt.distro)
+			tt.expectErr(t, err)
+			if err != nil {
+				return
+			}
+
+			if diff := cmp.Diff(tt.expected, result, cmpopts.EquateEmpty()); diff != "" {
+				t.Errorf("unexpected result (-want +got):\n%s", diff)
 			}
 		})
 	}
@@ -348,4 +580,16 @@ func testNonDistroAffectedPackage2Handle() *AffectedPackageHandle {
 			CVEs: []string{"CVE-2023-4567"},
 		},
 	}
+}
+
+func expectErrIs(t *testing.T, expected error) require.ErrorAssertionFunc {
+	t.Helper()
+	return func(t require.TestingT, err error, msgAndArgs ...interface{}) {
+		require.Error(t, err, msgAndArgs...)
+		assert.ErrorIs(t, err, expected)
+	}
+}
+
+func strRef(s string) *string {
+	return &s
 }
