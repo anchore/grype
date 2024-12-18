cbd45420ab4d14ef0db53afeaa25e4aba4e4b1a4
Alex Goodman <wagoodman@users.noreply.github.com>
2024-12-18 15:22:16 -0500

    add schema drift checks
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>

---
+++ .github/scripts/json-schema-drift-check.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -u
+
+if [ "$(git status --porcelain | wc -l)" -ne "0" ]; then
+  echo "  🔴 there are uncommitted changes, please commit them before running this check"
+  exit 1
+fi
+
+if ! make generate-json-schema; then
+  echo "Generating json schema failed"
+  exit 1
+fi
+
+if [ "$(git status --porcelain | wc -l)" -ne "0" ]; then
+  echo "  🔴 there are uncommitted changes, please commit them before running this check"
+  exit 1
+fi
