437c5075bdb0dc81d92af5e83e345fa4136674c2
Keith Zantow <kzantow@gmail.com>
2025-02-05 16:11:45 -0500

    chore: cleanup
    Signed-off-by: Keith Zantow <kzantow@gmail.com>

--- grype/match/provider.go
+++ grype/match/provider.go
@@ -1,26 +1,5 @@
 package match
 
-import (
-	"fmt"
-
-	"github.com/anchore/grype/grype/pkg"
-	"github.com/anchore/grype/grype/vulnerability"
-	syftPkg "github.com/anchore/syft/syft/pkg"
-)
-
-var ErrUnsupportedPackageType = fmt.Errorf("unsupported package type")
-
-// Matcher is the interface to implement to provide top-level package-to-match
-type Matcher interface {
-	PackageTypes() []syftPkg.Type
-
-	Type() MatcherType
-
-	// Match is called for every package found, returning any matches and an optional Ignorer which will be applied
-	// after all matches are found
-	Match(vp vulnerability.Provider, p pkg.Package) ([]Match, []IgnoredMatch, error)
-}
-
 type ExclusionProvider interface {
 	IgnoreRules(vulnerabilityID string) ([]IgnoreRule, error)
 }
