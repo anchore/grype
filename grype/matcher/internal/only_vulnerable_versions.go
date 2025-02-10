3f53af56f99b65737e82ca4255a3b27b22c09c8e
Keith Zantow <kzantow@gmail.com>
2025-02-10 18:32:17 -0500

    chore: check for nil
    Signed-off-by: Keith Zantow <kzantow@gmail.com>

--- grype/matcher/internal/only_vulnerable_versions.go
+++ grype/matcher/internal/only_vulnerable_versions.go
@@ -8,5 +8,10 @@ import (
 
 // onlyVulnerableVersion returns a criteria object that tests affected vulnerability ranges against the provided version
 func onlyVulnerableVersions(v *version.Version) vulnerability.Criteria {
+	if v == nil {
+		return search.ByFunc(func(v vulnerability.Vulnerability) (bool, error) {
+			return true, nil
+		})
+	}
 	return search.ByVersion(*v)
 }
