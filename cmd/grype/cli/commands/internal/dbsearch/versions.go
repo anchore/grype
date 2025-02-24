73393bd7abb69a044b69469dd051bfd6f8f724cf
Alex Goodman <wagoodman@users.noreply.github.com>
2025-02-24 15:22:13 -0500

    update JSON schema
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>

--- cmd/grype/cli/commands/internal/dbsearch/versions.go
+++ cmd/grype/cli/commands/internal/dbsearch/versions.go
@@ -2,8 +2,16 @@ package dbsearch
 
 const (
 	// MatchesSchemaVersion is the schema version for the `db search ` command
-	MatchesSchemaVersion = "1.0.0"
+	MatchesSchemaVersion = "1.0.1"
+
+	// MatchesSchemaVersion Changelog:
+	// 1.0.0 - Initial schema 🎉
+	// 1.0.1 - Add KEV and EPSS data to vulnerability matches
 
 	// VulnerabilitiesSchemaVersion is the schema version for the `db search vuln` command
-	VulnerabilitiesSchemaVersion = "1.0.0"
+	VulnerabilitiesSchemaVersion = "1.0.1"
+
+	// VulnerabilitiesSchemaVersion
+	// 1.0.0 - Initial schema 🎉
+	// 1.0.1 - Add KEV and EPSS data to vulnerability
 )
