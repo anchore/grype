ee6d91d8e0d3d8254d54d78398237724ba9f7f66
Alex Goodman <wagoodman@users.noreply.github.com>
2025-03-19 08:53:05 -0400

    add v5 namespace emulation to db search output (#2539)
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>

--- cmd/grype/cli/commands/internal/dbsearch/versions.go
+++ cmd/grype/cli/commands/internal/dbsearch/versions.go
@@ -2,11 +2,12 @@ package dbsearch
 
 const (
 	// MatchesSchemaVersion is the schema version for the `db search ` command
-	MatchesSchemaVersion = "1.0.1"
+	MatchesSchemaVersion = "1.0.2"
 
 	// MatchesSchemaVersion Changelog:
 	// 1.0.0 - Initial schema 🎉
 	// 1.0.1 - Add KEV and EPSS data to vulnerability matches
+	// 1.0.2 - Add v5 namespace emulation for affected packages
 
 	// VulnerabilitiesSchemaVersion is the schema version for the `db search vuln` command
 	VulnerabilitiesSchemaVersion = "1.0.1"
