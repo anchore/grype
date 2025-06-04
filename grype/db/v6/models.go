f01d9af4bbb8688177c2e774aaa54a390edc3a08
Weston Steimel <author@code.w.steimel.me.uk>
2025-06-04 13:53:41 +0100

    fix: valid cpes in db search output
    Signed-off-by: Weston Steimel <author@code.w.steimel.me.uk>

--- grype/db/v6/models.go
+++ grype/db/v6/models.go
@@ -675,7 +675,7 @@ type Cpe struct {
 }
 
 func (c Cpe) String() string {
-	parts := []string{"cpe:2.3", c.Part, c.Vendor, c.Product, c.Edition, c.Language, c.SoftwareEdition, c.TargetHardware, c.TargetSoftware, c.Other}
+	parts := []string{"cpe:2.3", c.Part, c.Vendor, c.Product, "*", "*", c.Edition, c.Language, c.SoftwareEdition, c.TargetHardware, c.TargetSoftware, c.Other}
 	for i, part := range parts {
 		if part == "" {
 			parts[i] = "*"
