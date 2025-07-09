10e970ca97b74e9590ddf246e0aad2ca578a127f
Alex Goodman <wagoodman@users.noreply.github.com>
2025-07-09 17:06:47 -0400

    output distro channels in json output
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>

--- grype/presenter/models/distribution.go
+++ grype/presenter/models/distribution.go
@@ -7,9 +7,10 @@ import (
 
 // distribution provides information about a detected Linux distribution.
 type distribution struct {
-	Name    string   `json:"name"`    // Name of the Linux distribution
-	Version string   `json:"version"` // Version of the Linux distribution (major or major.minor version)
-	IDLike  []string `json:"idLike"`  // the ID_LIKE field found within the /etc/os-release file
+	Name     string   `json:"name"`               // Name of the Linux distribution
+	Version  string   `json:"version"`            // Version of the Linux distribution (major or major.minor version)
+	IDLike   []string `json:"idLike"`             // the ID_LIKE field found within the /etc/os-release file
+	Channels []string `json:"channels,omitempty"` // channels for the distribution, if available
 }
 
 // newDistribution creates a struct with the Linux distribution to be represented in JSON.
@@ -23,9 +24,10 @@ func newDistribution(ctx pkg.Context, d *distro.Distro) distribution {
 	}
 
 	return distribution{
-		Name:    d.Name(),
-		Version: d.Version,
-		IDLike:  cleanIDLike(d.IDLike),
+		Name:     d.Name(),
+		Version:  d.Version,
+		IDLike:   cleanIDLike(d.IDLike),
+		Channels: d.Channels,
 	}
 }
 
