d1bef423a7be8fa71bb5505aa80b1f0dacb2e8a8
Alex Goodman <wagoodman@users.noreply.github.com>
2025-08-20 08:52:07 -0400

    revert vuln schema change
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>

--- cmd/grype/cli/commands/internal/dbsearch/versions.go
+++ cmd/grype/cli/commands/internal/dbsearch/versions.go
@@ -12,11 +12,10 @@ const (
 	// 1.1.0 - Add fix available date information to vulnerability range object. This removes existing unused git-commit and date fields from the schema, but is a non-breaking change.
 
 	// VulnerabilitiesSchemaVersion is the schema version for the `db search vuln` command
-	VulnerabilitiesSchemaVersion = "1.1.0"
+	VulnerabilitiesSchemaVersion = "1.0.3"
 
 	// VulnerabilitiesSchemaVersion
 	// 1.0.0 - Initial schema 🎉
 	// 1.0.1 - Add KEV and EPSS data to vulnerability
 	// 1.0.3 - Add severity string field to vulnerability object
-	// 1.1.0 - Add fix available date information to vulnerability range object. This removes existing unused git-commit and date fields from the schema, but is a non-breaking change.
 )
