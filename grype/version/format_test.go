e266f46feeb7678206a27db6f0a73afdeed1cbbe
Alex Goodman <wagoodman@users.noreply.github.com>
2025-09-02 21:27:36 -0400

    parse version formats by purl types
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>

--- grype/version/format_test.go
+++ grype/version/format_test.go
@@ -10,26 +10,7 @@ func TestParseFormat(t *testing.T) {
 		input  string
 		format Format
 	}{
-		{
-			input:  "dpkg",
-			format: DebFormat,
-		},
-		{
-			input:  "bitnami",
-			format: BitnamiFormat,
-		},
-		{
-			input:  "maven",
-			format: MavenFormat,
-		},
-		{
-			input:  "gem",
-			format: GemFormat,
-		},
-		{
-			input:  "deb",
-			format: DebFormat,
-		},
+		// SemanticFormat cases
 		{
 			input:  "semantic",
 			format: SemanticFormat,
@@ -37,6 +18,134 @@ func TestParseFormat(t *testing.T) {
 		{
 			input:  "semver",
 			format: SemanticFormat,
+		},
+		{
+			input:  "npm",
+			format: SemanticFormat,
+		},
+		{
+			input:  "nuget",
+			format: SemanticFormat,
+		},
+		{
+			input:  "composer",
+			format: SemanticFormat,
+		},
+		{
+			input:  "hex",
+			format: SemanticFormat,
+		},
+		{
+			input:  "pub",
+			format: SemanticFormat,
+		},
+		{
+			input:  "swift",
+			format: SemanticFormat,
+		},
+		{
+			input:  "conan",
+			format: SemanticFormat,
+		},
+		{
+			input:  "cocoapods",
+			format: SemanticFormat,
+		},
+		{
+			input:  "hackage",
+			format: SemanticFormat,
+		},
+		// ApkFormat cases
+		{
+			input:  "apk",
+			format: ApkFormat,
+		},
+		// BitnamiFormat cases
+		{
+			input:  "bitnami",
+			format: BitnamiFormat,
+		},
+		// DebFormat cases
+		{
+			input:  "deb",
+			format: DebFormat,
+		},
+		{
+			input:  "dpkg",
+			format: DebFormat,
+		},
+		// GolangFormat cases
+		{
+			input:  "golang",
+			format: GolangFormat,
+		},
+		{
+			input:  "go",
+			format: GolangFormat,
+		},
+		// MavenFormat cases
+		{
+			input:  "maven",
+			format: MavenFormat,
+		},
+		// RpmFormat cases
+		{
+			input:  "rpm",
+			format: RpmFormat,
+		},
+		// PythonFormat cases
+		{
+			input:  "python",
+			format: PythonFormat,
+		},
+		{
+			input:  "pypi",
+			format: PythonFormat,
+		},
+		{
+			input:  "pep440",
+			format: PythonFormat,
+		},
+		// KBFormat cases
+		{
+			input:  "kb",
+			format: KBFormat,
+		},
+		// GemFormat cases
+		{
+			input:  "gem",
+			format: GemFormat,
+		},
+		// PortageFormat cases
+		{
+			input:  "portage",
+			format: PortageFormat,
+		},
+		// JVMFormat cases
+		{
+			input:  "jvm",
+			format: JVMFormat,
+		},
+		{
+			input:  "jre",
+			format: JVMFormat,
+		},
+		{
+			input:  "jdk",
+			format: JVMFormat,
+		},
+		{
+			input:  "openjdk",
+			format: JVMFormat,
+		},
+		{
+			input:  "jep223",
+			format: JVMFormat,
+		},
+		// UnknownFormat case
+		{
+			input:  "unknown",
+			format: UnknownFormat,
 		},
 	}
 
