e36e904ce8abc6dac791f83d263511f07f541138
Will Murphy <willmurphyscode@users.noreply.github.com>
2025-09-29 15:14:48 -0400

    chore: add db schema validation
    Add a method for validating when models or blogs change in the v6
    schema. Generate two schema files, a JSON schema describing the
    validation of blobs in the DB, and a SQL schema describing the GORM
    models.
    
    Signed-off-by: Will Murphy <willmurphyscode@users.noreply.github.com>

---
+++ .github/scripts/db-schema-drift-check.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -u
+
+if [ "$(git status --porcelain | wc -l)" -ne "0" ]; then
+  echo "  🔴 there are uncommitted changes, please commit them before running this check"
+  exit 1
+fi
+
+if ! task generate-db-schema; then
+  echo "Generating database blob schemas failed"
+  exit 1
+fi
+
+if [ "$(git status --porcelain | wc -l)" -ne "0" ]; then
+  echo "  🔴 database blob schemas have uncommitted changes"
+  echo "  Run 'task generate-db-schema' and commit the changes"
+  echo ""
+  git status --porcelain
+  echo ""
+  git diff schema/grype/db/
+  exit 1
+fi
+
+echo "✅ Database blob schemas are up to date"
