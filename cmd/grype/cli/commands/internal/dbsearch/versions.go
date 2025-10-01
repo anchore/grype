0e53506473bceb01caf068295ed89a6a689943cb
Will Murphy <willmurphyscode@users.noreply.github.com>
2025-10-01 13:19:06 -0400

    bump db and db search schema versions
    Signed-off-by: Will Murphy <willmurphyscode@users.noreply.github.com>

--- cmd/grype/cli/commands/internal/dbsearch/versions.go
+++ cmd/grype/cli/commands/internal/dbsearch/versions.go
@@ -2,7 +2,7 @@ package dbsearch
 
 const (
 	// MatchesSchemaVersion is the schema version for the `db search` command
-	MatchesSchemaVersion = "1.1.1"
+	MatchesSchemaVersion = "1.1.2"
 
 	// MatchesSchemaVersion Changelog:
 	// 1.0.0 - Initial schema 🎉
@@ -11,12 +11,14 @@ const (
 	// 1.0.3 - Add severity string field to vulnerability object
 	// 1.1.0 - Add fix available date information to vulnerability range object. This removes existing unused git-commit and date fields from the schema, but is a non-breaking change.
 	// 1.1.1 - Add unaffected package and unaffected cpe to output
+	// 1.1.2 - Add CWE IDs to vulnerability output
 
 	// VulnerabilitiesSchemaVersion is the schema version for the `db search vuln` command
-	VulnerabilitiesSchemaVersion = "1.0.3"
+	VulnerabilitiesSchemaVersion = "1.0.4"
 
 	// VulnerabilitiesSchemaVersion
 	// 1.0.0 - Initial schema 🎉
 	// 1.0.1 - Add KEV and EPSS data to vulnerability
 	// 1.0.3 - Add severity string field to vulnerability object
+	// 1.0.4 - Add CWE IDs to vulnerability output
 )
