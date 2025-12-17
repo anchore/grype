648bfe44e7f26180b11f522028e9f9124c853d5b
Alex Goodman <wagoodman@users.noreply.github.com>
2025-12-17 14:17:51 -0500

    use specifier matching for python version (#3121)
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>

--- grype/version/pep440_version.go
+++ grype/version/pep440_version.go
@@ -11,12 +11,26 @@ type pep440Version struct {
 }
 
 func newPep440Version(raw string) (pep440Version, error) {
+	// lets ensure this is a valid PEP 440 version
 	parsed, err := goPepVersion.Parse(raw)
 	if err != nil {
 		return pep440Version{}, invalidFormatError(SemanticFormat, raw, err)
 	}
+
+	// we want to use the "public" portion of the version for comparison purposes (for specifier matching, not local versions).
+	// Note per PEP 440:
+	//   <public version identifier>[+<local version label>]
+	// see:
+	// - https://peps.python.org/pep-0440/#public-version-identifiers
+	// - https://peps.python.org/pep-0440/#local-version-identifiers
+	//
+	// This means that for a version like "1.0.0+abc.1", we only want to consider "1.0.0" for comparison purposes.
+	public, err := goPepVersion.Parse(parsed.Public())
+	if err != nil {
+		return pep440Version{}, invalidFormatError(SemanticFormat, raw, err)
+	}
 	return pep440Version{
-		obj: parsed,
+		obj: public,
 	}, nil
 }
 
