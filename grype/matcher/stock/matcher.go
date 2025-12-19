6cbf3af61f95bee4a12541e7d4fd7b76840fef0d
Rez Moss <hi@rezmoss.com>
2025-12-19 16:44:57 -0500

    fixed false positives for ubuntu kernel packages and backfixes like CVE-2023-2163
    Signed-off-by: Rez Moss <hi@rezmoss.com>

--- grype/matcher/stock/matcher.go
+++ grype/matcher/stock/matcher.go
@@ -1,12 +1,17 @@
 package stock
 
 import (
+	"github.com/anchore/grype/grype/distro"
 	"github.com/anchore/grype/grype/match"
 	"github.com/anchore/grype/grype/matcher/internal"
 	"github.com/anchore/grype/grype/pkg"
 	"github.com/anchore/grype/grype/vulnerability"
 	syftPkg "github.com/anchore/syft/syft/pkg"
 )
+
+var mainDistros = []distro.Type{
+	distro.Ubuntu,
+}
 
 type Matcher struct {
 	cfg MatcherConfig
@@ -31,5 +36,23 @@ func (m *Matcher) Type() match.MatcherType {
 }
 
 func (m *Matcher) Match(store vulnerability.Provider, p pkg.Package) ([]match.Match, []match.IgnoreFilter, error) {
+	//skip cpe matching for linux-kernel packages on major distros like ubuntu to avoid false positives from nvd's version ranges not covering backported fixes
+	//kernel vulns are still found accurately using dpkg/rpm matchers with distro data that includes backported fixes
+	if p.Type == syftPkg.LinuxKernelPkg && isMainDistro(p.Distro) {
+		return nil, nil, nil
+	}
+
 	return internal.MatchPackageByEcosystemAndCPEs(store, p, m.Type(), m.cfg.UseCPEs)
+}
+
+func isMainDistro(d *distro.Distro) bool {
+	if d == nil {
+		return false
+	}
+	for _, comprehensive := range mainDistros {
+		if d.Type == comprehensive {
+			return true
+		}
+	}
+	return false
 }
