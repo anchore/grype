4a1676af4ea5582183a773665d4ab56fea800e98
Will Murphy <willmurphyscode@users.noreply.github.com>
2026-07-08 14:36:17 -0400

    feat(rhel): duplicate RHSAs to all applicable RHEL minor versions (#3542)
    * feat(rhel): duplicate RHSAs to all applicable RHEL minor versions
    
    Historically, affected package handles for RHEL have been emitted at
    major versions only, modeling RHEL 9, for example, as a single namespace
    within which to search for vulnerabilities. However, this representation
    cannot correctly match on situations where multiple RHSAs fix the same
    RPM on the same (CVE, RHEL major, RPM name) tuple. In order to create
    this capability without necessitating a client side matching behavior
    change, implement the following workaround: copy RHEL affected and
    unaffected package handles to all RHEL minor versions within the
    applicable major, causing grype to find a fix at RHEL 9.2, for example,
    instead of just RHEL 9. In this way, when there are multiple RHSAs for
    the same RHEL major, they can be copied to the RHEL minors that are
    applicable, allowing Grype to pick the correct fixed version.
    
    So in the example of CVE-2023-38545 for curl, an affected package handle
    for RHSA-2023:5763's version is emitted for rhel 9.0, 9.1, and 9.2
    namespaces, and for RHSA-2023:6745's version is emitted for 9.3 and all
    subsequent 9.x minor versions, effectively gaining the ability to
    improve matching on older Grype versions at the expense of increased
    database size.
    
    In the rare case where there are multiple applicable RHSAs _and_ one of
    them cannot be assigned to a set of RHEL minor versions, guess the set
    of RHEL minor versions based on EVR ordering with the other RHSAs.
    
    Signed-off-by: Will Murphy <willmurphyscode@users.noreply.github.com>
    
    * rhel multi-RHSA: fix minor version advisory plumbing
    
    Signed-off-by: Will Murphy <willmurphyscode@users.noreply.github.com>
    
    * add eus stream handling test case
    
    Signed-off-by: Will Murphy <willmurphyscode@users.noreply.github.com>
    
    * add generator for set of rhel minors
    
    Signed-off-by: Will Murphy <willmurphyscode@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Will Murphy <willmurphyscode@users.noreply.github.com>
    Signed-off-by: Christopher Phillips <32073428+spiffcs@users.noreply.github.com>

---
+++ grype/db/v6/build/transformers/os/generate.go
@@ -0,0 +1,7 @@
+package os
+
+// rhelMinorSpan (rhel_minor_span_generated.go) is generated from Red Hat's official
+// product life-cycle API; see generate/main.go. Values change only when Red Hat
+// announces a new major (or retires one), so regeneration is an infrequent,
+// reviewed event rather than a scheduled chore.
+//go:generate go run ./generate
