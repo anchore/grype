c45fc81a2427e8cde5148ea57d506d81846088e7
Keith Zantow <kzantow@gmail.com>
2026-08-20 09:36:56 -0400

    chore: update the stdin subprocess tests
    Signed-off-by: Keith Zantow <kzantow@gmail.com>

--- test/cli/testdata/image-node-subprocess/app.js
+++ test/cli/testdata/image-node-subprocess/app.js
@@ -1,6 +1,6 @@
-require("child_process").spawn("grype", [
+const child = require("child_process").spawn("grype", [
     "-vv",
-    "registry:busybox:latest",
+    "sbom:/sbom.json",
 ], {
   // we want to see any output from stdout/stderr which is why they are inherited from the parent process.
   // The real test is to make certain that piped input will not hang forever when nothing is provided on stdin
@@ -8,3 +8,6 @@ require("child_process").spawn("grype", [
   // as the only indicator to expect analysis input from stdin.
   stdio: ["pipe", "inherit", "inherit"]
 });
+
+// surface grype's exit code so the test can tell a successful scan from a failed one
+child.on("close", (code) => process.exit(code === null ? 1 : code));
--- test/cli/utils_test.go
+++ test/cli/utils_test.go
@@ -2,6 +2,7 @@ package cli
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"io"
 	"os"
@@ -14,6 +15,7 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	"github.com/anchore/grype/internal/dbtest"
 	"github.com/anchore/stereoscope/pkg/imagetest"
 )
 
@@ -28,14 +30,16 @@ func getGrypeCommand(tb testing.TB, args ...string) *exec.Cmd {
 	tb.Helper()
 	argsWithConfig := args
 	if !grypeCommandHasConfigArg(argsWithConfig...) {
+		// note: this is an absolute path since the working directory may have been changed by
+		// building the snapshot binary
 		argsWithConfig = append(
-			[]string{"-c", "../grype-test-config.yaml"},
+			[]string{"-c", filepath.Join(repoRoot(tb), "test", "grype-test-config.yaml")},
 			args...,
 		)
 	}
 
 	return exec.Command(
-		getGrypeSnapshotLocation(tb, runtime.GOOS),
+		getGrypeSnapshotLocation(tb, runtime.GOOS, runtime.GOARCH),
 		argsWithConfig...,
 	)
 }
@@ -49,24 +53,28 @@ func grypeCommandHasConfigArg(args ...string) bool {
 	return false
 }
 
-func getGrypeSnapshotLocation(t testing.TB, goOS string) string {
+func getGrypeSnapshotLocation(t testing.TB, goOS, goArch string) string {
+	loc := getGrypeBinaryLocationByOS(t, goOS, goArch)
 	// GRYPE_BINARY_LOCATION is the absolute path to the snapshot binary
 	const envKey = "GRYPE_BINARY_LOCATION"
 	if os.Getenv(envKey) != "" {
-		return os.Getenv(envKey)
+		// we were run from a snapshot test, so we already have a snapshot binary
+		return loc
 	}
-	loc := getGrypeBinaryLocationByOS(t, goOS)
-	buildBinary(t, loc)
-	_ = os.Setenv(envKey, loc)
+	// if this was built locally
+	if os.Getenv(loc) != "" {
+		return loc
+	}
+	buildBinary(t, loc, goOS, goArch)
+	_ = os.Setenv(loc, loc)
 	return loc
 }
 
-func getGrypeBinaryLocationByOS(t testing.TB, goOS string) string {
+func getGrypeBinaryLocationByOS(t testing.TB, goOS, goArch string) string {
 	// note: for amd64 we need to update the snapshot location with the v1 suffix
 	// see : https://goreleaser.com/customization/build/#why-is-there-a-_v1-suffix-on-amd64-builds
-	archPath := runtime.GOARCH
-	if runtime.GOARCH == "amd64" {
-		archPath = fmt.Sprintf("%s_v1", archPath)
+	if goArch == "amd64" {
+		goArch = fmt.Sprintf("%s_v1", goArch)
 	}
 	executable := "grype"
 	// note: there is a subtle - vs _ difference between these versions
@@ -75,33 +83,65 @@ func getGrypeBinaryLocationByOS(t testing.TB, goOS string) string {
 		executable += ".exe"
 		fallthrough
 	case "darwin", "linux":
-		return filepath.Join(repoRoot(t), "snapshot", fmt.Sprintf("%s-build_%s_%s", goOS, goOS, archPath), executable)
+		return filepath.Join(repoRoot(t), "snapshot", fmt.Sprintf("%s-build_%s_%s", goOS, goOS, goArch), executable)
 	default:
 		t.Fatalf("unsupported OS: %s", runtime.GOOS)
 	}
 	return ""
 }
 
-func buildBinary(t testing.TB, loc string) {
-	t.Chdir(repoRoot(t))
-	t.Log("Building grype...")
+func buildBinary(t testing.TB, loc, goOS, goArch string) {
+	t.Logf("Building grype for %s %s...", goOS, goArch)
 	c := exec.Command("go", "build", "-o", loc, "./cmd/grype")
+	// note: set the build directory on the command instead of changing the working directory of the
+	// test process itself -- tests (and imagetest fixtures) resolve paths relative to ./test/cli
+	c.Dir = repoRoot(t)
+	environ := append(os.Environ(), "CGO_ENABLED=0")
+	if goOS != runtime.GOOS || goArch != runtime.GOARCH {
+		environ = append(environ, fmt.Sprintf("GOOS=%s", goOS), fmt.Sprintf("GOARCH=%s", goArch))
+	}
+	c.Env = environ
 	c.Stdout = os.Stdout
 	c.Stderr = os.Stderr
 	c.Stdin = os.Stdin
 	require.NoError(t, c.Run())
 }
 
-func getDockerRunCommand(tb testing.TB, args ...string) *exec.Cmd {
+// getDockerRunCommandContext gets a command to run docker that allows interrupting the container,
+// which allows --rm to clean it up instead of leaving it running
+func getDockerRunCommandContext(ctx context.Context, tb testing.TB, args ...string) *exec.Cmd {
 	tb.Helper()
 
-	return exec.Command(
+	cmd := exec.CommandContext(
+		ctx,
 		"docker",
 		append(
 			[]string{"run"},
 			args...,
 		)...,
 	)
+	cmd.Cancel = func() error {
+		// docker forwards this to the container, which lets it (and --rm) shut down cleanly
+		return cmd.Process.Signal(os.Interrupt)
+	}
+	cmd.WaitDelay = 10 * time.Second
+
+	return cmd
+}
+
+// seedTestDB installs a locally built vulnerability DB into a new directory and returns that
+// directory, so that tests can run grype without downloading the real DB.
+func seedTestDB(t *testing.T) string {
+	t.Helper()
+
+	dbDir := t.TempDir()
+	cmd, stdout, stderr := runGrype(t, map[string]string{
+		"GRYPE_DB_CACHE_DIR":  dbDir,
+		"GRYPE_DB_UPDATE_URL": dbtest.NewServer(t).Start(),
+	}, "db", "update", "-v")
+	assertSucceedingReturnCode(t, stdout, stderr, cmd.ProcessState.ExitCode())
+
+	return dbDir
 }
 
 func runGrype(tb testing.TB, env map[string]string, args ...string) (*exec.Cmd, string, string) {
@@ -172,19 +212,5 @@ func assertCommandExecutionSuccess(t testing.TB, cmd *exec.Cmd) {
 		}
 
 		t.Fatalf("unable to run command %q: %v", cmd, err)
-	}
-}
-
-func testWithTimeout(t *testing.T, name string, timeout time.Duration, test func(*testing.T)) {
-	done := make(chan bool)
-	go func() {
-		t.Run(name, test)
-		done <- true
-	}()
-
-	select {
-	case <-time.After(timeout):
-		t.Fatal("test timed out")
-	case <-done:
 	}
 }
